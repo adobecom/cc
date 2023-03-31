5d8d59dae376ef3b65cc58920c3b5430258e8f17
mathuria <mathuria@adobe.com>
2023-03-31 21:52:35 +0530

    Integrating review changes

--- cc/blocks/promotion/promotion.js
+++ cc/blocks/promotion/promotion.js
@@ -1,14 +1,8 @@
 export default async function init(el, promotionPath = '/creativecloud/promotions/hub') {
   const promotionName = el.getAttribute('data-promotion');
   const response = await window.fetch(`${promotionPath}/${promotionName}.plain.html`);
-  if (!response.ok) {
-    return;
-  }
+  if (!response.ok) return;
   const promotionContent = await response.text();
-  if (!promotionContent.length) {
-    return;
-  }
-
-  el.appendChild(document.createRange()
-    .createContextualFragment(promotionContent));
+  if (!promotionContent.length) return;
+  el.appendChild(document.createRange().createContextualFragment(promotionContent));
 }
