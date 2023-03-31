130cb918012a6a966fe22fc06bff6acf0087ef51
mathuria <mathuria@adobe.com>
2023-03-31 21:48:00 +0530

    Integrating review changes

--- cc/features/article-promotion.js
+++ cc/features/article-promotion.js
@@ -1,6 +1,6 @@
 export default function init(el) {
   const promo = document.createElement('div');
   promo.classList.add('promotion');
-  promo.setAttribute('data-promotion', el.getAttribute('content').toLowerCase());
+  promo.setAttribute('data-promotion', el.toLowerCase());
   document.querySelector('main > div').appendChild(promo);
 }
