5aeb6886d29a97d07b788ed98933cccaf404ca34
mathuria <mathuria@adobe.com>
2023-03-31 21:56:54 +0530

    Integrating review changes

--- test/features/article-promotion.test.js
+++ test/features/article-promotion.test.js
@@ -8,7 +8,7 @@ describe('short form article promotion when metadata is set', () => {
   before(async () => {
     const metaEl = document.querySelector('head meta[name="promotion"]');
     const { default: init } = await import('../../cc/features/article-promotion.js');
-    await init(metaEl);
+    await init(metaEl.content);
   });
 
   it('adds promotion article content', async () => {
