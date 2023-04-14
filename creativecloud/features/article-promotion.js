11a76c5b698ed1485e05efcd3e4172a90dc02846
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2023-04-14 10:38:19 -0700

    Update article-promotion.js

--- creativecloud/features/article-promotion.js
+++ creativecloud/features/article-promotion.js
@@ -1,6 +1,6 @@
 export default function init(el) {
   const promo = document.createElement('div');
   promo.classList.add('promotion');
-  promo.setAttribute('data-promotion', el.toLowerCase());
+  if (el !=== null) promo.setAttribute('data-promotion', el.toLowerCase());
   document.querySelector('main > div').appendChild(promo);
 }
