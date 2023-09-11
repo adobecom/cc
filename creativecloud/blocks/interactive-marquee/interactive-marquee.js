3d78fcd7a85a4a84c50f2152750967a9dcc2cad0
Suhani Jain <suhjain@Suhanis-MacBook-Pro.local>
2023-09-11 19:11:02 +0530

    putting background first

--- creativecloud/blocks/interactive-marquee/interactive-marquee.js
+++ creativecloud/blocks/interactive-marquee/interactive-marquee.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable max-len */
 import miloLibs from '../../scripts/scripts.js';
 
@@ -19,10 +20,106 @@ const decorateBlockBg = (node) => {
 
   [...children].forEach(async (child, index) => {
     if (childCount === 3) {
-      child.classList.add(viewports[index]);
+      child.classList.add(viewports[index], 'blade');
     }
   });
 };
+
+function getImageSrc(node) {
+  return Array.from(node).map((el) => {
+    const a = el.querySelector('picture > img').src;
+    console.log('a: ', a, el);
+    return a;
+  });
+}
+
+function getText(node) {
+  return Array.from(node).map((el) => el.innerText.trim());
+}
+
+function getValue(childrenArr, type) {
+  const valueArr = type === 'img' ? getImageSrc(childrenArr) : getText(childrenArr);
+  let newData = [];
+  if (valueArr.length === 1) {
+    newData = [valueArr[0], valueArr[0], valueArr[0]];
+  } else if (valueArr.length === 2) {
+    newData = [valueArr[0], valueArr[0], valueArr[1]];
+  } else {
+    newData = [valueArr[0], valueArr[1], valueArr[2]];
+  }
+  return newData;
+}
+
+function processDataSet(dataSet) {
+  const { children } = dataSet;
+  const childrenArr = [...children];
+  return childrenArr;
+}
+
+function getImageUrlValues(dataSet, objKeys, viewportType) {
+  let childrenArr = '';
+  if (objKeys === 'defaultBgSrc') {
+    childrenArr = processDataSet(dataSet[0]);
+  } else if (objKeys === 'talentSrc') {
+    childrenArr = processDataSet(dataSet[1]);
+  } else if (objKeys === 'marqueeTitleImgSrc') {
+    childrenArr = processDataSet(dataSet[2]);
+  }
+  const getValueArr = getValue(childrenArr, 'img');
+  console.log('getValueArr: ',viewportType, getValueArr);
+  if (viewportType === 'desktop') {
+    return getValueArr[2];
+  } else if (viewportType === 'tablet') {
+    return getValueArr[1];
+  }
+  return getValueArr[0];
+}
+
+function getTextItemValues(dataSet, viewportType, flag) {
+  const childrenArr = processDataSet(dataSet);
+  if (flag) {
+    childrenArr.shift();
+  }
+  const getValueArr = getValue(childrenArr, 'text');
+  if (viewportType === 'desktop') {
+    return getValueArr[2];
+  } else if (viewportType === 'tablet') {
+    return getValueArr[1];
+  }
+  return getValueArr[0];
+}
+
+function getIconAndName(dataSet, viewportType) {
+  const childrenArr = processDataSet(dataSet);
+  const objArr = [];
+  const iconBlock = childrenArr.shift();
+  objArr['iconUrl'] = iconBlock.querySelector('picture > img').src;
+  objArr['name'] = getTextItemValues(dataSet, viewportType, true);
+  return objArr;
+}
+
+async function createConfig(el) {
+  customElem.config = configObj;
+  const dataSet = el.querySelectorAll(':scope > div');
+  for (const viewportType of ['mobile', 'tablet', 'desktop']) {
+    const viewportObj = {};
+    for (const objKeys of ['defaultBgSrc', 'talentSrc', 'marqueeTitleImgSrc']) {
+      viewportObj[objKeys] = getImageUrlValues(dataSet, objKeys, viewportType);
+    }
+    viewportObj['tryitText'] = getTextItemValues(dataSet[3], viewportType);
+    viewportObj['groups'] = [];
+    for (let i = 4; i < dataSet.length - 1; i++) {
+      const arr = getIconAndName(dataSet[i], viewportType);
+      viewportObj.groups.push({'iconUrl': arr.iconUrl, 'name': arr.name});
+    }
+    // TODO: uncomment when needed
+    configObj[viewportType] = viewportObj;
+  }
+  // customElem.config = configObj;
+  excelLink = dataSet[dataSet.length - 1].innerText.trim();
+  el.innerText = '';
+  el.appendChild(customElem);
+}
 
 export default async function init(el) {
   import(`${base}/deps/blades/interactivemarquee.js`);
@@ -31,4 +128,5 @@ export default async function init(el) {
     dataSet[0].classList.add('background');
     decorateBlockBg(dataSet[0]);
   }
+  createConfig(el);
 }
