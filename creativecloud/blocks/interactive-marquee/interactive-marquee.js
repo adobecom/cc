77d1562b46c13a583b382087773285b360263ac5
Suhani Jain <suhjain@Suhanis-MacBook-Pro.local>
2023-09-13 15:52:48 +0530

    interactive-marquee block

--- creativecloud/blocks/interactive-marquee/interactive-marquee.js
+++ creativecloud/blocks/interactive-marquee/interactive-marquee.js
@@ -1,162 +1,34 @@
 // /* eslint-disable no-restricted-syntax */
 // /* eslint-disable max-len */
-// import miloLibs from '../../scripts/scripts.js';
-
-// const { getConfig } = await import(`${miloLibs}/utils/utils.js`);
-// const customElem = document.createElement('ft-changebackgroundmarquee');
-// let excelLink = '';
-// const configObj = {};
-// const config = getConfig();
-// const base = config.codeRoot;
-
-// const decorateBlockBg = (node) => {
-//   const viewports = ['mobile-only', 'tablet-only', 'desktop-only'];
-//   const { children } = node;
-//   const childCount = node.childElementCount;
-//   if (childCount === 2) {
-//     children[0].classList.add(viewports[0], viewports[1]);
-//     children[1].classList.add(viewports[2]);
-//   }
-
-//   [...children].forEach(async (child, index) => {
-//     if (childCount === 3) {
-//       child.classList.add(viewports[index], 'blade');
-//     }
-//   });
-//   console.log('1---------------');
-// };
-
-// function getImageSrc(node) {
-//   return Array.from(node).map((el) => {
-//     const a = el.querySelector('picture > img').src;
-//     return a;
-//   });
-// }
-
-// function getText(node) {
-//   return Array.from(node).map((el) => el.innerText.trim());
-// }
-
-// function getValue(childrenArr, type) {
-//   const valueArr = type === 'img' ? getImageSrc(childrenArr) : getText(childrenArr);
-//   let newData = [];
-//   if (valueArr.length === 1) {
-//     newData = [valueArr[0], valueArr[0], valueArr[0]];
-//   } else if (valueArr.length === 2) {
-//     newData = [valueArr[0], valueArr[0], valueArr[1]];
-//   } else {
-//     newData = [valueArr[0], valueArr[1], valueArr[2]];
-//   }
-//   return newData;
-// }
-
-// function processDataSet(dataSet) {
-//   const { children } = dataSet;
-//   const childrenArr = [...children];
-//   return childrenArr;
-// }
-
-// function getImageUrlValues(dataSet, objKeys, viewportType) {
-//   let childrenArr = '';
-//   if (objKeys === 'defaultBgSrc') {
-//     childrenArr = processDataSet(dataSet[0]);
-//   } else if (objKeys === 'talentSrc') {
-//     childrenArr = processDataSet(dataSet[1]);
-//   } else if (objKeys === 'marqueeTitleImgSrc') {
-//     childrenArr = processDataSet(dataSet[2]);
-//   }
-//   const getValueArr = getValue(childrenArr, 'img');
-//   if (viewportType === 'desktop') {
-//     return getValueArr[2];
-//   } else if (viewportType === 'tablet') {
-//     return getValueArr[1];
-//   }
-//   return getValueArr[0];
-// }
-
-// function getTextItemValues(dataSet, viewportType, flag) {
-//   const childrenArr = processDataSet(dataSet);
-//   if (flag) {
-//     childrenArr.shift();
-//   }
-//   const getValueArr = getValue(childrenArr, 'text');
-//   if (viewportType === 'desktop') {
-//     return getValueArr[2];
-//   } else if (viewportType === 'tablet') {
-//     return getValueArr[1];
-//   }
-//   return getValueArr[0];
-// }
-
-// function getIconAndName(dataSet, viewportType) {
-//   const childrenArr = processDataSet(dataSet);
-//   const objArr = [];
-//   const iconBlock = childrenArr.shift();
-//   objArr['iconUrl'] = iconBlock.querySelector('picture > img').src;
-//   objArr['name'] = getTextItemValues(dataSet, viewportType, true);
-//   return objArr;
-// }
-
-// async function createConfig(el) {
-//   customElem.config = configObj;
-//   const dataSet = el.querySelectorAll(':scope > div');
-//   for (const viewportType of ['mobile', 'tablet', 'desktop']) {
-//     const viewportObj = {};
-//     for (const objKeys of ['defaultBgSrc', 'talentSrc', 'marqueeTitleImgSrc']) {
-//       viewportObj[objKeys] = getImageUrlValues(dataSet, objKeys, viewportType);
-//     }
-//     viewportObj['tryitText'] = getTextItemValues(dataSet[3], viewportType);
-//     viewportObj['groups'] = [];
-//     for (let i = 4; i < dataSet.length - 1; i++) {
-//       const arr = getIconAndName(dataSet[i], viewportType);
-//       viewportObj.groups.push({'iconUrl': arr.iconUrl, 'name': arr.name});
-//     }
-//     // TODO: uncomment when needed
-//     configObj[viewportType] = viewportObj;
-//   }
-//   // customElem.config = configObj;
-//   excelLink = dataSet[dataSet.length - 1].innerText.trim();
-//   console.log('2---------------------');
-// }
-
-// export default async function init(el) {
-//   const clone = el.cloneNode(true);
-//   const background = document.createElement('div');
-//   background.classList.add('background');
-//   const dataSet = el.querySelectorAll(':scope > div');
-//   if (dataSet.length > 1) {
-//     background.appendChild(dataSet[0]);
-//     background.appendChild(dataSet[1]);
-//     background.appendChild(dataSet[2]);
-//     decorateBlockBg(dataSet[0]);
-//     decorateBlockBg(dataSet[1]);
-//     decorateBlockBg(dataSet[2]);
-//     el.appendChild(background);
-//   }
-//   import(`${base}/deps/blades/interactivemarquee.js`);
-//   createConfig(clone);
-//   el.innerText = '';
-//   el.appendChild(customElem);
-// }
-
-
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable no-self-assign */
-/* eslint-disable prefer-destructuring */
 import miloLibs from '../../scripts/scripts.js';
 
 const { getConfig } = await import(`${miloLibs}/utils/utils.js`);
 const customElem = document.createElement('ft-changebackgroundmarquee');
-let excelJsonData = '';
 let excelLink = '';
 const configObj = {};
 const config = getConfig();
 const base = config.codeRoot;
 
+const decorateBlockBg = (node) => {
+  const viewports = ['mobile-only', 'tablet-only', 'desktop-only'];
+  const { children } = node;
+  const childCount = node.childElementCount;
+  if (childCount === 2) {
+    children[0].classList.add(viewports[0], viewports[1]);
+    children[1].classList.add(viewports[2]);
+  }
+
+  [...children].forEach(async (child, index) => {
+    if (childCount === 3) {
+      child.classList.add(viewports[index], 'blade');
+    }
+  });
+  console.log('1---------------');
+};
+
 function getImageSrc(node) {
   return Array.from(node).map((el) => {
     const a = el.querySelector('picture > img').src;
-    console.log('a: ', a, el);
     return a;
   });
 }
@@ -194,7 +66,6 @@ function getImageUrlValues(dataSet, objKeys, viewportType) {
     childrenArr = processDataSet(dataSet[2]);
   }
   const getValueArr = getValue(childrenArr, 'img');
-  console.log('getValueArr: ',viewportType, getValueArr);
   if (viewportType === 'desktop') {
     return getValueArr[2];
   } else if (viewportType === 'tablet') {
@@ -226,69 +97,6 @@ function getIconAndName(dataSet, viewportType) {
   return objArr;
 }
 
-export async function getExcelData(excelLink) {
-  const resp = await fetch(`${excelLink}`);
-  const html1 = await resp.json();
-  console.log("🚀 ~ file: interactive-marquee.js:90 ~ getExcelData ~ html1:", html1)
-  const { data } = html1;
-  const arr = [];
-  data.forEach((grp) => {
-    arr.push(grp);
-  });
-  return arr;
-}
-
-function getExcelDataCursor(excelJson, type) {
-  const foundData = excelJson.find((data) => data.MappedName === type);
-  if (foundData) {
-    return foundData.Value1;
-  }
-  return null;
-}
-
-function getSrcFromExcelData(name, viewportType, excelData, type) {
-  const arr = [];
-  excelData.forEach((data) => {
-    if (data.ComponentName === name && data.Viewport === viewportType && data.MappedName === type) {
-      if (data.Value1.trim() !== '') {
-        arr.push(data.Value1);
-      }
-      if (data.Value2.trim() !== '') {
-        arr.push(data.Value2);
-      }
-      if (data.Value3.trim() !== '') {
-        arr.push(data.Value3);
-      }
-    }
-  });
-  return arr;
-}
-
-function createConfigExcel(excelJson, configObjData) {
-  for (const viewportType of ['desktop', 'tablet', 'mobile']) {
-    configObjData[`${viewportType}`].tryitSrc = getExcelDataCursor(excelJson, 'tryitSrc');
-    configObjData[`${viewportType}`].cursorSrc = getExcelDataCursor(excelJson, 'cursorSrc');
-    const existingGroups = configObjData[`${viewportType}`].groups;
-    for (const group of existingGroups) {
-      const name = group.name;
-      const groupsrc = getSrcFromExcelData(name, viewportType, excelJson, 'src');
-      const groupswatchSrc = getSrcFromExcelData(name, viewportType, excelJson, 'swatchSrc');
-      if (groupsrc.length > 0 && groupswatchSrc.length > 0) {
-        group.options = [];
-        for (let i = 0; i < groupsrc.length; i++) {
-          group.options.push({'src': groupsrc[i], 'swatchSrc': groupswatchSrc[i]});
-        }
-      }
-      if (groupsrc.length > 0 && groupswatchSrc.length === 0) {
-        group.options = [];
-        for (let i = 0; i < groupsrc.length; i++) {
-          group.options.push({'src': groupsrc[i]});
-        }
-      }
-    }
-  }
-}
-
 async function createConfig(el) {
   customElem.config = configObj;
   const dataSet = el.querySelectorAll(':scope > div');
@@ -308,17 +116,22 @@ async function createConfig(el) {
   }
   // customElem.config = configObj;
   excelLink = dataSet[dataSet.length - 1].innerText.trim();
+  console.log('2---------------------');
 }
 
 export default async function init(el) {
-  console.log(el);
   const clone = el.cloneNode(true);
-  // el.innerText = '';
-  el.appendChild(customElem);
-  createConfig(clone);
+  const dataSet = el.querySelectorAll(':scope > div');
+  if (dataSet.length > 1) {
+    dataSet[0].classList.add('background');
+    dataSet[2].classList.add('background');
+    decorateBlockBg(dataSet[0]);
+    decorateBlockBg(dataSet[2]);
+  }
   import(`${base}/deps/blades/interactivemarquee.js`);
-  // console.log('configObjq1', customElem.config);
-  excelJsonData = await getExcelData(excelLink);
-  createConfigExcel(excelJsonData, configObj);
-  console.log('configObj2', customElem.config);
+  createConfig(clone);
+  setTimeout(() => {
+    el.innerText = '';
+    el.appendChild(customElem);
+  }, 1500);
 }
