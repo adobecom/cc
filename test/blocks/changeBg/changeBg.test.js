c0e6402eee2028f15b50181b13d0c10d9a3b0b53
Suhani Jain <suhjain@Suhanis-MacBook-Pro.local>
2023-11-15 00:07:48 +0530

    Interactive marquee

---
+++ test/blocks/changeBg/changeBg.test.js
@@ -0,0 +1,14 @@
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+import sinon, { stub } from 'sinon';
+import changeBg from '../../../creativecloud/features/changeBg/changeBg.js';
+
+document.body.innerHTML = await readFile({ path: './mocks/index.html' });
+describe('interactive-marquee', async () => {
+  const marquee = document.querySelector('.changebg');
+  await changeBg(marquee);
+  it('should exist', () => {
+    const changebackgroundmarquee = document.querySelector('ft-changebackgroundmarquee');
+    expect(changebackgroundmarquee).to.exist;
+  });
+});
