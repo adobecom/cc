1f05a2ec5b6c75fee8db292a072d664ce95180c7
Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>
2023-11-15 17:58:46 -0800

    lint issue

--- creativecloud/blocks/interactive-marquee/interactive-marquee.js
+++ creativecloud/blocks/interactive-marquee/interactive-marquee.js
@@ -75,13 +75,14 @@ function interactiveInit(el) {
 export default async function init(el) {
   interactiveInit(el);
   switch (true) {
-    case el.classList.contains('firefly'):
+    case el.classList.contains('firefly'): {
       const buttons = el.querySelectorAll('.con-button');
       if (buttons.length === 0) return;
       buttons.forEach((button) => { if (button.innerText.includes('Firefly')) button.setAttribute('daa-ll', 'getfirefly'); });
-      const { setInteractiveFirefly } = await import('../../features/firefly/firefly-interactive.js');
+      const { default: setInteractiveFirefly } = await import('../../features/firefly/firefly-interactive.js');
       setInteractiveFirefly(el);
       break;
+    } 
     default:
       // default case
       break;
