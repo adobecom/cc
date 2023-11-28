43a5e50093fb2aadc0a516fa0a214c1a3e45fba0
Suhani Jain <suhjain@Suhanis-MacBook-Pro.local>
2023-11-28 09:38:43 +0530

    chnages in authoring feedback

--- creativecloud/features/changeBg/author-feedback.js
+++ creativecloud/features/changeBg/author-feedback.js
@@ -1,3 +1,8 @@
+document.getElementsByTagName('head')[0].insertAdjacentHTML(
+  'beforeend',
+  '<link rel="stylesheet" href="/creativecloud/features/changeBg/author-feedback.css" />',
+);
+
 const LCP_SECTION_TITLES = ['background', 'foreground', 'text'];
 const enticement = ['tryit', 'cursor'];
 let rowID = 0;
@@ -14,6 +19,18 @@ const THUMBNAILS_DIMENSIONS = [
   [70, 70], // image 3 thumbnail
 ];
 
+const notificationsContainer = document.createElement('div');
+notificationsContainer.className = 'notifications';
+const errors = [];
+
+function notify(message, className) {
+  const messageContainer = document.createElement('div');
+  messageContainer.innerHTML = message;
+  messageContainer.className = className;
+
+  notificationsContainer.append(messageContainer);
+}
+
 function analyze(el) {
   const rows = el.querySelectorAll(':scope > div');
   function checkHexColor(rowIdx) {
@@ -23,7 +40,8 @@ function analyze(el) {
       const colors = cell.innerText.split(',');
       colors.forEach((color) => {
         if (!color.match(/^#[0-9A-Fa-f]{6}$/)) {
-          console.log(`bad color format at row ${rowIdx}; format should be #rrggbb, found '${color}'`);
+          errors.push(`bad color format at row ${rowIdx}; format should be #rrggbb, found '${color}'`);
+          notify(errors[errors.length - 1], 'error');
         }
       });
     });
@@ -32,9 +50,11 @@ function analyze(el) {
   function checkExnticement(rowIdx) {
     const row = rows[rowIdx];
     if (!row.querySelector('a') && rowIdx === 3) {
-      console.log(`Expecting tryit text and link at row${rowIdx}`);
+      errors.push(`Expecting tryit text and link at row${rowIdx}`);
+      notify(errors[errors.length - 1], 'error');
     } else if (!row.querySelector('a') && rowIdx === 4) {
-      console.log(`Expecting Cursor text and link at row${rowIdx}`);
+      errors.push(`Expecting Cursor text and link at row${rowIdx}`);
+      notify(errors[errors.length - 1], 'error');
     }
   }
 
@@ -46,12 +66,14 @@ function analyze(el) {
       const pictures = cell.querySelectorAll('picture');
 
       if (!dimensions[colIdx] && pictures.length > 1) {
-        console.log(`row ${rowIdx}, col ${colIdx} should be empty`);
+        errors.push(`row ${rowIdx}, col ${colIdx} should be empty`);
+        notify(errors[errors.length - 1], 'error');
         return;
       }
 
       if (dimensions[colIdx] && (pictures.length === 0)) {
-        console.log(`expected an image in row ${rowIdx}, col ${colIdx}`);
+        errors.push(`expected an image in row ${rowIdx}, col ${colIdx}`);
+        notify(errors[errors.length - 1], 'error');
         return;
       }
       pictures.forEach((picture) => {
@@ -69,14 +91,14 @@ function analyze(el) {
             const { width } = img;
             const { height } = img;
             if (width !== dimensions[colIdx][0] || height !== dimensions[colIdx][1]) {
-              console.log(`wrong image size in row ${rowIdx}, col ${colIdx}: ${[img.width, img.height]}, expecting ${dimensions[colIdx]}`);
+              errors.push(`wrong image size in row ${rowIdx}, col ${colIdx}: ${[img.width, img.height]}, expecting ${dimensions[colIdx]}`);
+              notify(errors[errors.length - 1], 'error');
             }
           };
         }
       });
     });
   }
-
   // check LCP images
   LCP_SECTION_TITLES.forEach((lcp, lcpIdx) => {
     const dimensions = [...IMAGE_DIMENSIONS];
@@ -111,6 +133,11 @@ function analyze(el) {
   }
 }
 
-export default function debug(el) {
-  analyze(el);
+export default async function debug(el) {
+  await analyze(el);
+  const debugContainer = document.createElement('div');
+  debugContainer.className = 'debug';
+  el.parentElement.append(debugContainer);
+  debugContainer.append(el);
+  debugContainer.append(notificationsContainer);
 }
