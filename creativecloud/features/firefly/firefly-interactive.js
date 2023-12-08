17ee0e4cf521e65c6009bc22cee5c5e7313bfce3
Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>
2023-12-07 21:56:43 -0800

    incorporate review comments

--- creativecloud/features/firefly/firefly-interactive.js
+++ creativecloud/features/firefly/firefly-interactive.js
@@ -95,10 +95,8 @@ export default async function setInteractiveFirefly(el) {
   const ttiDetail = {};
   const genfDetail = {};
   const teDetail = {};
-  const fireflyDomain = [
-    'firefly.adobe.com',
-  ];
-  const allSelections = [...media.querySelectorAll('p:not(:empty)')].filter((p) => p.innerText.trim().toLowerCase().includes(fireflyDomain[0]));
+  const fireflyDomain = 'firefly.adobe.com';
+  const allSelections = [...media.querySelectorAll('p:not(:empty)')].filter((p) => p.innerText.trim().toLowerCase().includes(fireflyDomain));
   allSelections.forEach((s) => {
     const optionPromptMode = interactiveElemMode;
     const selectorValues = s.innerText.split('|');
