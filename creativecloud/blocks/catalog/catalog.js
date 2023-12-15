ff7a8c419edb7ac515ad1bd598b7a8e49f093b72
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2023-12-15 16:21:43 +0100

    eagerly load all spectrum dependencies

--- creativecloud/blocks/catalog/catalog.js
+++ creativecloud/blocks/catalog/catalog.js
@@ -3,14 +3,25 @@ import { getLibs } from '../../scripts/utils.js';
 import '../sidenav/sidenav.js';
 
 export default async function init(el) {
-  const libs = getLibs();
+  const miloLibs = getLibs();
   await Promise.all([
-    import(`${libs}/deps/lit-all.min.js`),
-    import(`${libs}/features/spectrum-web-components/dist/sidenav.js`),
-    import(`${libs}/deps/merch-cards.js`),
-    import(`${libs}/blocks/merch-cards/merch-cards.js`),
-    import(`${libs}/deps/merch-card.js`),
-    import(`${libs}/blocks/merch-card/merch-card.js`),
+    import(`${miloLibs}/deps/lit-all.min.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/theme.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/button.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/icons/checkmark.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/icons/chevron.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/help-text.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/icon.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/icons-ui.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/icons-workflow.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/menu.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/overlay.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/popover.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/reactive-controllers.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/search.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/shared.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/sidenav.js`),
+    import(`${miloLibs}/features/spectrum-web-components/dist/textfield.js`),
   ]);
   el.classList.add('app');
   el.innerHTML = '';
