aac0a71addc44e2126023eaa143368c143de558f
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2023-12-15 11:38:58 +0100

    preload sidenav and merch-cards

--- creativecloud/blocks/catalog/catalog.js
+++ creativecloud/blocks/catalog/catalog.js
@@ -1,4 +1,13 @@
+import '../../deps/merch-sidenav.js';
+import { getLibs } from '../../scripts/utils.js';
+import '../sidenav/sidenav.js';
+
 export default async function init(el) {
+  const libs = getLibs();
+  await Promise.all([
+    import(`${libs}/blocks/merch-cards/merch-cards.js`),
+    import(`${libs}/deps/merch-cards.js`),
+  ]);
   el.classList.add('app');
   el.innerHTML = '';
   return el;
