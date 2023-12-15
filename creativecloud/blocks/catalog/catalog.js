6a1478c1ed17a5e207bb1a9c327f830ddf750bc0
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2023-12-15 13:05:32 +0100

    optimize loading of dependencies

--- creativecloud/blocks/catalog/catalog.js
+++ creativecloud/blocks/catalog/catalog.js
@@ -5,8 +5,12 @@ import '../sidenav/sidenav.js';
 export default async function init(el) {
   const libs = getLibs();
   await Promise.all([
-    import(`${libs}/blocks/merch-cards/merch-cards.js`),
+    import(`${libs}/deps/lit-all.min.js`),
+    import(`${libs}/features/spectrum-web-components/dist/sidenav.js`),
     import(`${libs}/deps/merch-cards.js`),
+    import(`${libs}/blocks/merch-cards/merch-cards.js`),
+    import(`${libs}/deps/merch-card.js`),
+    import(`${libs}/blocks/merch-card/merch-card.js`),
   ]);
   el.classList.add('app');
   el.innerHTML = '';
