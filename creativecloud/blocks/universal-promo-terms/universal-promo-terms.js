e513bb20fd63286a3f40c2be5e5e46a48d9f1e24
Sean Choi <seanchoi@adobe.com>
2024-01-30 09:28:56 -0700

    Added API failure case handler

--- creativecloud/blocks/universal-promo-terms/universal-promo-terms.js
+++ creativecloud/blocks/universal-promo-terms/universal-promo-terms.js
@@ -1,6 +1,6 @@
-const OFFER_ID_API_BASE = 'https://aos-stage.adobe.io/offers/02CBBA6AB01085A7F70EDA1A17521394';
+const OFFER_ID_API_BASE = 'https://aos-stage.adobe.io/offers/';
 const SELECTOR_ID_API_BASE = 'https://aos-stage.adobe.io/offers:search.selector';
-const STAGE_OFFER_ID_API_BASE = 'https://aos-stage.adobe.io/offers/02CBBA6AB01085A7F70EDA1A17521394';
+const STAGE_OFFER_ID_API_BASE = 'https://aos-stage.adobe.io/offers/';
 const STAGE_SELECTOR_ID_API_BASE = 'https://aos-stage.adobe.io/offers:search.selector';
 
 function searchToObject() {
@@ -21,22 +21,41 @@ function searchToObject() {
 
 function getEnv(env) {
   if (env) return env;
-  if (window.location.origin.includes('localshot') || window.location.origin.includes('hlx')) return 'stage';
+  if (window.location.origin.includes('localhost') || window.location.origin.includes('hlx')) return 'stage';
   return 'production';
+}
+
+async function getTermsHTML(search) {
+  const env = getEnv(search.env);
+  console.log(search);
+  if (!search.offer_selector_ids) {
+    const res = await fetch(`${env === 'stage' ? STAGE_OFFER_ID_API_BASE : OFFER_ID_API_BASE}${search.offer_id}?${window.location.search}`);
+    if (!res.ok) return false;
+    const json = await res.json();
+    const promoTerms = json[0]?.promo_terms;
+    if (!promoTerms.header || !promoTerms.text) {
+      return false;
+    }
+    return `<div class="container"><h1>${promoTerms.header}</h1><p>${promoTerms.text}</p></div>`;
+  } else {
+    const res = await fetch(`${env === 'stage' ? STAGE_SELECTOR_ID_API_BASE : SELECTOR_ID_API_BASE}${window.location.search}`);
+    if (!res.ok) return false;
+    const json = await res.json();
+    const promoTerms = json[0]?.offers[0]?.promo_terms;
+    if (!promoTerms.header || !promoTerms.text) {
+      return false;
+    }
+    return `<div class="container"><h1>${promoTerms.header}</h1><p>${promoTerms.text}</p></div>`;
+  }
 }
 
 export default async function init(el) {
   const search = searchToObject();
   const env = getEnv(search.env);
-  if (!search.offer_selector_ids) {
-    const res = await fetch(`${env === 'stage' ? STAGE_OFFER_ID_API_BASE : OFFER_ID_API_BASE}${window.location.search}`);
-    const json = await res.json();
-    const promoTerms = json[0].promo_terms;
-    el.innerHTML = `<div class="container"><h1>${promoTerms.header}</h1><p>${promoTerms.text}</p></div>`;
+  const termsHTML = await getTermsHTML(search);
+  if(!termsHTML && env !== 'stage') {
+    window.location = '404.html';
   } else {
-    const res = await fetch(`${env === 'stage' ? STAGE_SELECTOR_ID_API_BASE : SELECTOR_ID_API_BASE}${window.location.search}`);
-    const json = await res.json();
-    const promoTerms = json[0].offers[0].promo_terms;
-    el.innerHTML = `<div class="container"><h1>${promoTerms.header}</h1><p>${promoTerms.text}</p></div>`;
+    el.innerHTML = termsHTML;
   }
 }
