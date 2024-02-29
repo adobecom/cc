518cfbac041da894973279d64a39483a4400ca10
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-02-29 12:06:34 +0100

    paralellize network and cpu

--- creativecloud/blocks/catalog/catalog.js
+++ creativecloud/blocks/catalog/catalog.js
@@ -3,16 +3,13 @@ import { getLibs } from '../../scripts/utils.js';
 /** container block */
 export default async function init(el) {
   const libs = getLibs();
-  const [merchCards, sidenav] = await Promise.all([
-    import(`${libs}/blocks/merch-cards/merch-cards.js`)
-      .then(({ default: initMerchCards }) => {
-        const merchCardsEl = el.querySelector('.merch-cards');
-        merchCardsEl.classList.add('four-merch-cards');
-        return initMerchCards(merchCardsEl);
-      }),
-    import('../sidenav/sidenav.js')
-      .then(({ default: initSidenav }) => initSidenav(el.querySelector('.sidenav'))),
-  ]);
+  const sidenavEl = el.querySelector('.sidenav');
+  const merchCardsEl = el.querySelector('.merch-cards');
+  merchCardsEl.classList.add('four-merch-cards');
+  const { default: initMerchCards } = await import(`${libs}/blocks/merch-cards/merch-cards.js`);
   el.innerHTML = '';
-  el.append(merchCards, sidenav);
+  el.append(await initMerchCards(merchCardsEl));
+  const { default: initSidenav } = await import('../sidenav/sidenav.js');
+  initSidenav({ default: initSidenav });
+  el.append(await initSidenav(sidenavEl));
 }
