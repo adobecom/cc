b6d789074e9e198b9bdf69a314a7cb0e9dd82b9c
Sean Choi <seanchoi@adobe.com>
2024-03-01 17:24:53 -0700

    [Universal promo terms] update env logic (#197)
    * stage condition added for env
    
    * check prod URL instead

--- creativecloud/blocks/universal-promo-terms/universal-promo-terms.js
+++ creativecloud/blocks/universal-promo-terms/universal-promo-terms.js
@@ -7,8 +7,8 @@ const SERVICE_PROVIDERS = 'PROMO_TERMS';
 
 function getEnv(env) {
   if (env) return env;
-  if (window.location.origin.includes('localhost') || window.location.origin.includes('hlx')) return 'stage';
-  return 'production';
+  if (window.location.origin.hostname === 'www.adobe.com') return 'production';
+  return 'stage';
 }
 
 async function getTermsHTML(params, el, env, search) {
