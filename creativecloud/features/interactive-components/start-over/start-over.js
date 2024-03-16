c900e76881b1cda0685c59c8e27204b42c214483
Aishwarya Mathuria <mathuria@adobe.com>
2024-03-17 01:33:48 +0530

    Dev branch | Initial commit for interactive metadata

---
+++ creativecloud/features/interactive-components/start-over/start-over.js
@@ -0,0 +1,22 @@
+import { getLibs } from '../../../scripts/utils.js';
+
+export default async function stepInit(data) {
+  const miloLibs = getLibs('/libs');
+  const { createTag } = await import(`${miloLibs}/utils/utils.js`);
+  data.target.classList.add('step-crop');
+  const pTags = data.config.querySelectorAll('p');
+  const layer = createTag('div', { class: `layer layer-${data.stepIndex}` });
+  const startOverCTA = createTag('a', { class: 'gray-button body-s next-step start-over-button' });
+  [...pTags].forEach((p) => {
+    const img = p.querySelector('img');
+    if (img) {
+      const picClone = img.closest('picture').cloneNode(true);
+      if (img.src.includes('.svg')) startOverCTA.prepend(picClone);
+      else data.target.querySelector('picture').replaceWith(picClone);
+    } else {
+      startOverCTA.innerHTML += p.textContent.trim();
+    }
+  });
+  layer.append(startOverCTA);
+  data.target.append(layer);
+}
