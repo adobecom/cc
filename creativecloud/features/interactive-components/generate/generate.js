9da7e118271a646b4d20dd0b68b8c7189048f9a3
Drashti Modasara <drashti@drashtis-mbp.corp.adobe.com>
2024-03-18 18:31:48 +0530

    Drashti changes - cleaned css

--- creativecloud/features/interactive-components/generate/generate.js
+++ creativecloud/features/interactive-components/generate/generate.js
@@ -1,32 +1,3 @@
-// import { getLibs } from '../../../scripts/utils.js';
-
-// export default async function stepInit(data) {
-//   const miloLibs = getLibs('/libs');
-//   const { createTag } = await import(`${miloLibs}/utils/utils.js`);
-
-
-//   const pTags = data.stepConfigs[data.stepIndex].querySelectorAll('p');
-//   const layer = createTag('div', { class: `layer layer-${data.stepIndex}` });
-//   const generateCTA = createTag('a', { class: 'gray-button body-s next-step generate-button' });
-//   [...pTags].forEach((p) => {
-//     const img = p.querySelector('img');
-//     if (img) {
-//       const picClone = img.closest('picture').cloneNode(true);
-//       if (img.src.includes('.svg')) generateCTA.prepend(picClone);
-//       else data.stepInfo.handleImageTransition(data);
-//     } else {
-//       generateCTA.innerHTML += p.textContent.trim();
-//     }
-//   });
-//   generateCTA.addEventListener('click', (e) => {
-//     data.el.dispatchEvent(new CustomEvent(data.nextStepEvent));
-//   });
-//   layer.append(generateCTA);
-//   data.target.append(layer);
-// }
-
-
-
 import { getLibs } from '../../../scripts/utils.js';
 
 export default async function stepInit(data) {
@@ -40,10 +11,10 @@ export default async function stepInit(data) {
   const svg = config.querySelector('img[src*=svg]');
   const svgClone = svg.cloneNode(true);
   svg.insertAdjacentElement('afterEnd', svgClone);
-  const outerDiv = createTag('div', { class: `mobile-genfill layer show-layer layer-${data.stepIndex}` });
+  const outerDiv = createTag('div', { class: `layer show-layer layer-${data.stepIndex}` });
   const genfillDiv = createTag('div', { class: 'genfill-prompt body-s' });
   const searchBar = createTag('div', { class: 'genfill-search' }, `${searchText}`);
-  const generateBtn = createTag('a', { class: `generate body-xl next-step` }, `${btnText}`);
+  const generateBtn = createTag('a', { class: `gray-button generate body-xl next-step` }, `${btnText}`);
   generateBtn.prepend(svg);
   genfillDiv.appendChild(searchBar);
   genfillDiv.appendChild(generateBtn);
