b2e007ac87d1f44b77d6ca229d8d8982db2ea511
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-03-20 16:25:26 +0100

    attempt to optimize perf

--- creativecloud/blocks/catalog/catalog.js
+++ creativecloud/blocks/catalog/catalog.js
@@ -12,21 +12,19 @@ export default async function init(el) {
   const libs = getLibs();
   const sidenavEl = el.querySelector('.sidenav');
   const merchCardsEl = el.querySelector('.merch-card-collection');
+  el.innerHTML = '';
   let merchCards;
   if (merchCardsEl) {
+    el.appendChild(merchCardsEl);
     merchCardsEl.classList.add('four-merch-cards');
     const { default: initMerchCards } = await import(`${libs}/blocks/merch-card-collection/merch-card-collection.js`);
     merchCards = await initMerchCards(merchCardsEl);
-  }
-  el.innerHTML = '';
-  if (merchCards) {
-    el.append(merchCards);
   }
   if (sidenavEl) {
     (merchCards?.updateComplete ?? Promise.resolve()).then(async () => {
       const { default: initSidenav } = await import('../sidenav/sidenav.js');
       const sidenav = await initSidenav(sidenavEl);
-      el.append(sidenav);
+      el.appendChild(sidenav);
       await sidenav.updateComplete;
       if (merchCards) {
         merchCards.sidenav = sidenav;
