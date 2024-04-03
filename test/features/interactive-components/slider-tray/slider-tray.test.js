2fbc6d8ff6e9eba9170fd8a70ea2a49a63414463
Suhani <suhjain@Suhanis-MacBook-Pro.local>
2024-04-03 16:55:51 +0530

    small change

--- test/features/interactive-components/slider-tray/slider-tray.test.js
+++ test/features/interactive-components/slider-tray/slider-tray.test.js
@@ -1,9 +1,6 @@
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
 import { setLibs } from '../../../../creativecloud/scripts/utils.js';
-import waitForElement from '../../../helpers/waitForElement.js';
-import sinon from 'sinon';
-import MockIntersectionObserver from './mocks/mockObserver.js';
 
 setLibs('/libs');
 
@@ -11,45 +8,38 @@ const { default: init } = await import('../../../../creativecloud/blocks/interac
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 
 describe('hue-sat-marquee', () => {
-    let ib = null;
-    let mockObserver;
-    const globalObject = typeof global !== 'undefined' ? global : window;
+  let ib = null;
 
-    before(async () => {
-      console.log('marquee', document.querySelector('.interactive-metadata'));
-      await init(document.querySelector('.interactive-metadata'));
-      ib = document.querySelector('.marquee');
-      window.IntersectionObserver = sinon.stub().callsFake(callback => {
-        mockObserver = new MockIntersectionObserver(callback);
-        return mockObserver;
-      });
-    });
-  
-  
-    it('interactive marquee should exist', () => {
-      const promptbar =  document.querySelector('.interactive-enabled');
-      expect(promptbar).to.exist;
-    });
-  
-    it('listen to hue-slider event', () => {
-      document.querySelector('.hue-input').dispatchEvent(new Event('input'));
-      setTimeout(() => expect(ib).to.equal(''), 200);
-    });
-
-    it('Tabbing on slider', () => {
-        document.dispatchEvent(new Event('keydown'));
-        document.querySelector('.hue-input').dispatchEvent(new Event('focus'));
-        const focusableEle = document.querySelector('.focusUploadButton');
-        expect(focusableEle).to.exist;
-    });
-
-    it('Tabbing on slider', () => {
-        document.dispatchEvent(new Event('keydown'));
-        document.querySelector('.hue-input').dispatchEvent(new Event('blur'));
-        const focusableEle = document.querySelector('.focusUploadButton');
-        expect(focusableEle).to.not.exist;
-    });
+  before(async () => {
+    console.log('marquee', document.querySelector('.interactive-metadata'));
+    await init(document.querySelector('.interactive-metadata'));
+    ib = document.querySelector('.marquee');
   });
+
+  it('interactive marquee should exist', () => {
+    const promptbar = document.querySelector('.interactive-enabled');
+    expect(promptbar).to.exist;
+  });
+
+  it('listen to hue-slider event', () => {
+    document.querySelector('.hue-input').dispatchEvent(new Event('input'));
+    setTimeout(() => expect(ib).to.equal(''), 200);
+  });
+
+  it('Tabbing on slider', () => {
+    document.dispatchEvent(new Event('keydown'));
+    document.querySelector('.hue-input').dispatchEvent(new Event('focus'));
+    const focusableEle = document.querySelector('.focusUploadButton');
+    expect(focusableEle).to.exist;
+  });
+
+  it('Tabbing on slider', () => {
+    document.dispatchEvent(new Event('keydown'));
+    document.querySelector('.hue-input').dispatchEvent(new Event('blur'));
+    const focusableEle = document.querySelector('.focusUploadButton');
+    expect(focusableEle).to.not.exist;
+  });
+});
 //     let ib = null;
 //     let mockObserver;
 //     before(async () => {
