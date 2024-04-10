2dd73f8c492a741be3a295bcbd19234420bacf7c
mathuria <mathuria@adobe.com>
2024-04-10 15:28:52 +0530

    Adding new workflows

--- creativecloud/blocks/interactive-metadata/interactive-metadata.js
+++ creativecloud/blocks/interactive-metadata/interactive-metadata.js
@@ -256,6 +256,9 @@ function getWorkFlowInformation(el) {
     'workflow-generate-crop': ['generate', 'selector-tray', 'crop', 'start-over'],
     'workflow-generate-repeat-crop': ['generate', 'selector-tray', 'generate', 'selector-tray', 'crop', 'start-over'],
     'workflow-hue-sat': ['slider-tray'],
+    'workflow-generate-select-generate': ['generate', 'selector-tray', 'generate', 'crop', 'start-over'],
+    'workflow-generate-selector': ['generate', 'selector-tray', 'generate', 'start-over'],
+    'workflow-generate-triple-selector': ['generate', 'selector-tray', 'generate', 'selector-tray', 'generate', 'selector-tray', 'start-over'],
   };
   const wfNames = Object.keys(intWorkFlowConfig);
   [...el.classList].forEach((cn) => { if (cn.match('workflow-')) wfName = cn; });
