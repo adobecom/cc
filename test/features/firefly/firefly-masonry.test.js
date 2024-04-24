531c3c0e32c225d3fe4763e789e42be3e7d25650
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-04-24 15:56:19 -0700

    wip

--- test/features/firefly/firefly-masonry.test.js
+++ test/features/firefly/firefly-masonry.test.js
@@ -1,21 +1,29 @@
-import { readFile } from '@web/test-runner-commands';
+import { readFile, setViewport } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
 import { setLibs } from '../../../creativecloud/scripts/utils.js';
 import waitForElement from '../../helpers/waitForElement.js';
 
 setLibs('/libs');
+
+const { default: init } = await import('../../../creativecloud/blocks/interactive-marquee/interactive-marquee.js');
+
 function delay(ms) {
   return new Promise((res) => { setTimeout(() => { res(); }, ms); });
 }
 
-const { default: init } = await import('../../../creativecloud/blocks/interactive-marquee/interactive-marquee.js');
-const { handleTouchDevice } = await import('../../../creativecloud/features/firefly/firefly-masonry.js');
-
 describe('firefly-masonry', () => {
+  //let clock;
   before(async () => {
     document.body.innerHTML = await readFile({ path: './mocks/masonry-body.html' });
+    //clock = sinon.useFakeTimers();
     await init(document.querySelector('.ff-masonry'));
   });
+
+  /*after(() => {
+    clock.restore();
+    sinon.restore();
+  });*/
 
   it('Prompt should exist', async () => {
     const promptbar = await waitForElement('.masonry-promptbar');
@@ -25,10 +33,6 @@ describe('firefly-masonry', () => {
   it('Prompt should be placed at proper place in interactive container', async () => {
     const fireflypromptbar = await waitForElement('.ff-masonry-prompt');
     expect(fireflypromptbar).to.exist;
-  });
-
-  it('should autocycle', async () => {
-    await delay(500);
   });
 
   it('grid media should exist', async () => {
@@ -41,15 +45,22 @@ describe('firefly-masonry', () => {
     expect(mobileLayout).to.exist;
   });
 
-  it('should handle touch device', async () => {
-    const imageContainer = document.querySelector('.image-container');
-    handleTouchDevice(imageContainer, 500);
-  });
-
   it('Enticement should exist', async () => {
     const enticementText = await waitForElement('.enticement-text');
     const enticementArrow = await waitForElement('.enticement-arrow');
     expect(enticementText).to.exist;
     expect(enticementArrow).to.exist;
+  });
+
+  it('should autocycle', async () => {
+    await setViewport({ width: 600, height: 100 });
+    await delay(1000);
+  });
+
+  it('should change image content opacity and navigate to link after two tap for touch device', async () => {
+    await setViewport({ width: 599, height: 100 });
+    const imageContainer = await waitForElement('.image-container');
+    imageContainer.querySelector('a').dispatchEvent(new Event('touchstart'));
+    expect(imageContainer.querySelector('.image-content').style.opacity).to.equal('1');
   });
 });
