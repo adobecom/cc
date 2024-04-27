af27ad90676dd044e501f611d443b1aa96f59ade
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-04-26 22:18:12 -0700

    unit test

--- test/features/firefly/firefly-masonry.test.js
+++ test/features/firefly/firefly-masonry.test.js
@@ -7,10 +7,6 @@ setLibs('/libs');
 
 const { default: init } = await import('../../../creativecloud/blocks/interactive-marquee/interactive-marquee.js');
 
-function delay(ms) {
-  return new Promise((res) => { setTimeout(() => { res(); }, ms); });
-}
-
 describe('firefly-masonry', () => {
   before(async () => {
     document.body.innerHTML = await readFile({ path: './mocks/masonry-body.html' });
@@ -43,14 +39,6 @@ describe('firefly-masonry', () => {
     expect(enticementText).to.exist;
     expect(enticementArrow).to.exist;
   });
-
-  it('media should autocycle in mobile view', async () => {
-    await setViewport({ width: 500, height: 800 });
-    while (!document.querySelector('.mobile-only a.preload')) {
-      // eslint-disable-next-line no-await-in-loop
-      await delay(500);
-    }
-  }).timeout(5500);
 
   it('should change image content opacity on one tap for touch device', async () => {
     await setViewport({ width: 599, height: 100 });
