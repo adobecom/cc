edb2fec7c526a17fa1254049c97fcccc63623230
mathuria <mathuria@adobe.com>
2024-05-17 17:35:36 +0530

    Latest changes

---
+++ creativecloud/features/progress-circle/progress-circle.js
@@ -0,0 +1,27 @@
+import { createTag, loadStyle } from '../../scripts/utils.js';
+
+async function loadProgressCss() {
+  const stylePromise = new Promise((resolve) => {
+    loadStyle('/creativecloud/features/progress-circle/progress-circle.css', resolve);
+  });
+  await stylePromise;
+}
+
+export default async function createprogressCircle() {
+  await loadProgressCss();
+  const pdom = `<div class="spectrum-ProgressCircle-track"></div>
+  <div class="spectrum-ProgressCircle-fills">
+    <div class="spectrum-ProgressCircle-fillMask1">
+      <div class="spectrum-ProgressCircle-fillSubMask1">
+        <div class="spectrum-ProgressCircle-fill"></div>
+      </div>
+    </div>
+    <div class="spectrum-ProgressCircle-fillMask2">
+      <div class="spectrum-ProgressCircle-fillSubMask2">
+        <div class="spectrum-ProgressCircle-fill"></div>
+      </div>
+    </div>
+  </div>`;
+  const prgc = createTag('div', { class: 'spectrum-ProgressCircle spectrum-ProgressCircle--indeterminate' }, pdom);
+  return prgc;
+}
