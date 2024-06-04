12655d80ce8d4bb9301dfe505a6fd82b72c8cc32
Aishwarya Mathuria <mathuria@adobe.com>
2024-06-04 15:43:05 +0530

    Update slider-tray.js

--- creativecloud/features/interactive-components/slider-tray/slider-tray.js
+++ creativecloud/features/interactive-components/slider-tray/slider-tray.js
@@ -102,7 +102,10 @@ function createUploadButton(details, picture, sliderTray, menu) {
   const labelBtn = createTag('a', { class: `uploadButton body-${currentVP === 'mobile' ? 'm' : 'xl'}` }, details);
   const analyticsHolder = createTag('div', { class: 'interactive-link-analytics-text' }, `${details}`);
   labelBtn.append(btn, analyticsHolder);
-  appendSVGToButton(picture, labelBtn);
+  const svg = `<div class='svg-icon-container'>
+  <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 379.661"><path fill-rule="nonzero" d="M153.764 151.353c-7.838-.333-13.409-2.935-16.619-7.822-8.724-13.076 3.18-25.997 11.443-35.099 23.441-25.725 80.888-87.554 92.454-101.162 8.768-9.693 21.25-9.693 30.017 0 11.948 13.959 72.287 78.604 94.569 103.628 7.731 8.705 17.292 20.579 9.239 32.633-3.287 4.887-8.798 7.489-16.636 7.822H310.65v96.177c0 12.558-10.304 22.868-22.871 22.868h-63.544c-12.572 0-22.871-10.294-22.871-22.868v-96.177h-47.6zm-153 97.863c-2.622-10.841 1.793-19.33 8.852-24.342a24.767 24.767 0 018.47-3.838c3.039-.738 6.211-.912 9.258-.476 8.585 1.232 16.409 6.775 19.028 17.616a668.81 668.81 0 014.56 20.165 1259.68 1259.68 0 013.611 17.72c4.696 23.707 8.168 38.569 16.924 45.976 9.269 7.844 26.798 10.55 60.388 10.55h254.297c31.012 0 47.192-2.965 55.706-10.662 8.206-7.418 11.414-21.903 15.564-44.131a1212.782 1212.782 0 013.628-18.807c1.371-6.789 2.877-13.766 4.586-20.811 2.619-10.838 10.438-16.376 19.023-17.616 3.02-.434 6.173-.256 9.212.474 3.071.738 5.998 2.041 8.519 3.837 7.05 5.007 11.457 13.474 8.855 24.294l-.011.046a517.834 517.834 0 00-4.181 18.988c-1.063 5.281-2.289 11.852-3.464 18.144l-.008.047c-6.124 32.802-11.141 55.308-27.956 71.112-16.565 15.572-42.513 22.159-89.473 22.159H131.857c-49.096 0-76.074-5.911-93.429-21.279-17.783-15.75-23.173-38.615-30.047-73.314-1.39-7.029-2.728-13.738-3.638-18.091-1.281-6.11-2.6-12.081-3.979-17.761z"/></svg></div>`;
+  labelBtn.innerHTML = svg + labelBtn.innerHTML;
+  // appendSVGToButton(picture, labelBtn);
   const clone = labelBtn.cloneNode(true);
   clone.classList.add('uploadButtonMobile');
   const mobileInput = clone.querySelector('.inputFile');
@@ -134,7 +137,9 @@ function createUploadPSButton(details, picture, layer) {
   const btn = createTag('a', { class: 'continueButton body-xl hide', tabindex: '0' }, details);
   const analyticsHolder = createTag('div', { class: 'interactive-link-analytics-text' }, `${details}`);
   btn.append(analyticsHolder);
-  appendSVGToButton(picture, btn);
+  const svg = `<div class='svg-icon-container'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 122.88 121.93" style="enable-background:new 0 0 122.88 121.93" xml:space="preserve"><g><path d="M8.33,0.02h29.41v20.6H20.36v80.7h82.1V84.79h20.36v37.14H0V0.02H8.33L8.33,0.02z M122.88,0H53.3l23.74,23.18l-33.51,33.5 l21.22,21.22L98.26,44.4l24.62,24.11V0L122.88,0z"/></g></svg></div>`;
+  btn.innerHTML = svg + btn.innerHTML;
+  // appendSVGToButton(picture, btn);
   layer.append(btn);
 }
 
