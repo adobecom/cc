86a41983aec6f452a372d2ceaca67c9b43f40006
adgaurgit <adgaur@adobe.com>
2024-07-23 14:43:03 +0530

    title attribute changed

--- test/blocks/sidenav/sidenav.test.html.js
+++ test/blocks/sidenav/sidenav.test.html.js
@@ -61,7 +61,7 @@ runTests(async () => {
       const sidenavEl = document.querySelector('.plans');
       const newRoot = await init(sidenavEl);
       expect(newRoot.tagName).to.equal('MERCH-SIDENAV');
-      expect(newRoot.title).to.equal('CATEGORIES');
+      expect(newRoot.sidenavTitle).to.equal('CATEGORIES');
       const search = newRoot.querySelector('sp-search');
       expect(search).to.be.null;
       const nestedItems = newRoot.querySelectorAll('sp-sidenav-item > sp-sidenav-item');
