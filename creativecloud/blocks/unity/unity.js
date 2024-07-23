aa14e880b4e25f580d5229fcadb27fb18c308f55
mathuria <mathuria@adobe.com>
2024-07-23 12:22:21 +0530

    Integrating unity

--- creativecloud/blocks/unity/unity.js
+++ creativecloud/blocks/unity/unity.js
@@ -1,16 +1,15 @@
 function getUnityLibs(prodLibs, project = 'unity') {
+  let libs = '';
   const { hostname, origin } = window.location;
   if (project === 'unity') { libs = `${origin}/unitylibs`; return libs; }
   if (!hostname.includes('hlx.page')
     && !hostname.includes('hlx.live')
     && !hostname.includes('localhost')) {
-    libs = prodLibs;
-    return libs;
+    return prodLibs;
   }
   const branch = new URLSearchParams(window.location.search).get('unitylibs') || 'main';
-  if (branch.indexOf('--') > -1) { libs = `https://${branch}.hlx.live/unitylibs`; return libs; }
-  libs = `https://${branch}--unity--adobecom.hlx.live/unitylibs`;
-  return libs;
+  if (branch.indexOf('--') > -1) return `https://${branch}.hlx.live/unitylibs`;
+  return `https://${branch}--unity--adobecom.hlx.live/unitylibs`;
 }
 
 export default async function init(el) {
