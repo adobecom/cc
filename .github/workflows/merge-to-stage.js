223528b5698fe5c92c322be4026a219e822ecf75
sharath kannan <shkannan@adobe.com>
2024-07-29 12:03:02 +0530

    month and date added to the title of stage to main PR

--- .github/workflows/merge-to-stage.js
+++ .github/workflows/merge-to-stage.js
@@ -1,6 +1,6 @@
 const STAGE = 'stage';
 const PROD = 'main';
-const PR_TITLE = '[Release] Stage to Main';
+const PR_TITLE = `[Release] Stage to Main ${new Date().toLocaleDateString("en-US", { month: "2-digit", day: "2-digit" })}`;
 const SEEN = {};
 let github, owner, repo;
 let body = `
@@ -227,7 +227,7 @@ const getPRs = async () => {
 const getStageToMainPR = () =>
   github.rest.pulls
     .list({ owner, repo, state: 'open', base: PROD })
-    .then(({ data } = {}) => data.find(({ title } = {}) => title === PR_TITLE))
+    .then(({ data } = {}) => data.find(({ title } = {}) => title.includes('[Release] Stage to Main')))
     .then((pr) => pr && addLabels({ pr, github, owner, repo }))
     .then((pr) => pr && addFiles({ pr, github, owner, repo }))
     .then((pr) => {
