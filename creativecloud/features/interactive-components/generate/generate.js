b1396f7ea2a991b178a71f745178a30dadd8bccc
Suhani <suhjain@Suhanis-MacBook-Pro.local>
2024-07-29 09:42:57 +0530

    adding an generative label in generate tray

--- creativecloud/features/interactive-components/generate/generate.js
+++ creativecloud/features/interactive-components/generate/generate.js
@@ -1,8 +1,12 @@
 import { createTag } from '../../../scripts/utils.js';
 
 function getGenerateConfig(data) {
-  const genCfg = data.stepConfigs[data.stepIndex].querySelector('ul, ol');
-  const lastp = data.stepConfigs[data.stepIndex].querySelector(':scope > div > p:last-child');
+  console.log('data', data);
+  let genCfg = null;
+  if (data.stepIndex !== 0) {
+    genCfg = data.stepConfigs[data.stepIndex].querySelector('ul, ol');
+  }
+  const lastp = data.stepConfigs[data.stepIndex].querySelector(':scope > div > :last-child');
   if (!genCfg) return lastp;
   const dpth = data.displayPath;
   const allCfgs = genCfg.querySelectorAll('li');
@@ -15,22 +19,38 @@ export default async function stepInit(data) {
   const config = data.stepConfigs[data.stepIndex];
   const layer = createTag('div', { class: `layer layer-${data.stepIndex}` });
   const gerenateCfg = getGenerateConfig(data);
+  const firstp = config.querySelector(':scope > div > ul:first-of-type') || null;
+  console.log('lastp', firstp, gerenateCfg);
+  const labelBtn = createTag('div', { class: 'label-text' });
+  if (firstp) {
+    const labelText = firstp.textContent.trim();
+    labelBtn.appendChild(document.createTextNode(labelText));
+    const firstsvg = firstp?.querySelector('img[src*=".svg"]')?.closest('picture');
+    if (firstsvg) {
+      firstsvg.insertAdjacentElement('afterend', firstsvg.cloneNode(true));
+      labelBtn.appendChild(firstsvg);
+    }
+  }
+  console.log('gerenateCfg', gerenateCfg);
   const [searchText, btnText] = gerenateCfg.textContent.trim().split('|');
   const genfillDiv = createTag('div', { class: 'generate-prompt-button body-m' });
   const searchBar = createTag('div', { class: 'generate-text' }, `${searchText}`);
   const searchBarContainer = createTag('div', { class: 'generate-text-container' }, searchBar);
   const generateBtn = createTag('a', { class: 'gray-button generate-button next-step', href: '#' });
   const analyticsHolder = createTag('div', { class: 'interactive-link-analytics-text' }, `${searchText} - `);
-  const svg = config.querySelector('img[src*=".svg"]')?.closest('picture');
-  if (svg) {
-    svg.insertAdjacentElement('afterend', svg.cloneNode(true));
-    generateBtn.appendChild(svg);
+  const lastsvg = config.querySelector('img[src*=".svg"]')?.closest('picture');
+  if (lastsvg) {
+    lastsvg.insertAdjacentElement('afterend', lastsvg.cloneNode(true));
+    generateBtn.appendChild(lastsvg);
   }
   generateBtn.appendChild(analyticsHolder);
   generateBtn.appendChild(document.createTextNode(btnText));
   genfillDiv.appendChild(searchBarContainer);
   genfillDiv.appendChild(generateBtn);
-  layer.appendChild(genfillDiv);
+  const generateLayer = createTag('div', { class: 'generate-layer' });
+  generateLayer.appendChild(genfillDiv);
+  labelBtn ? generateLayer.appendChild(labelBtn) : '';
+  layer.appendChild(generateLayer);
   generateBtn.addEventListener('click', async (e) => {
     e.preventDefault();
     if (layer.classList.contains('disable-click')) return;
