a8c1045b693323d8dc5fd65c6d17b3e674003720
Mariia Lukianets <mariia.lukianets@gmail.com>
2024-07-30 11:49:20 +0200

    fix auth

--- .github/preview-index/README.md
+++ .github/preview-index/README.md
@@ -1,6 +1,57 @@
+# Usage
+
 1. Populate /preview-index/.env
 2. npm i
 3. npm run index
 
 
 If you do a change to .env file, remember to re-run 'npm i' before running 'npm run index' command.
+
+
+# Configuration
+Application expects a .env file of this format:
+```
+SP_CLIENT_ID="..."
+SP_TENANT_ID="..."
+SP_CLIENT_SECRET="..."
+SP_DRIVE_ID="..."
+EDS_ADMIN_KEY="..."
+EDS_ACCESS_KEY="..."
+CONSUMER="..."
+PREVIEW_INDEX_FILE="..."
+PREVIEW_RESOURCES_FOLDER="..."
+```
+
+`SP_CLIENT_ID` and `SP_TENANT_ID` can be found on the azure app 'Essential' tab, see 'Application (client) ID' and 'Directory (tenant) ID'.
+`PREVIEW_INDEX_FILE` path to the target index file, e.g. "milo/drafts/mariia/preview-index/query-index-cards-preview.xlsx"
+`PREVIEW_RESOURCES_FOLDER` path to resource to index, e.g. "/drafts/mariia/preview-index/*". Folder path is not sharepoint path, but mapped *hlx.page location. So if your sharepoint folder is CC/www/cc-shared/myfolder, please specify /cc-shared/myfolder.
+
+Azure app: [CC preview index](https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/94136756-61af-4f63-af05-6991a719b872/isMSAApp~/false)
+
+
+# CURLs
+
+Change `access_token` for an actual one.
+
+## EDS Admin API: Get previewed resources - bulk status
+Admin token and access token are different values!
+
+```
+curl -v -X POST --header "Authorization: Bearer admin_token" -H "Content-Type: application/json" -d '{"select": ["preview"], "paths": ["/drafts/mariia/preview-index/*"]}' 'https://admin.hlx.page/status/adobecom/cc/main/*'
+```
+
+## EDS Get page content
+Admin token and access token are different values!
+```
+curl -v --header "Authorization: token access_token" -H "Content-Type: application/json" 'https://main--cc--adobecom.hlx.page/cc-shared/fragments/merch/products/catalog/merch-card/ec/target/default'
+```
+
+## GRAPH API: Get index file Item ID
+change DRIVER_ID to the actual one
+```
+curl --header "Authorization: Bearer access_token" 'https://graph.microsoft.com/v1.0/drives/DRIVER_ID/root:/Book1.xlsx'
+```
+## GRAPH API: delete all rows
+```
+curl -X POST --header "Authorization: Bearer access_token" -H "Content-Type: application/json" -d '{"shift": "Up"}' 'https://graph.microsoft.com/v1.0/drives/${DRIVE_ID}/items/${ITEM_ID}/workbook/worksheets/raw_index/tables/Table1/DataBodyRange/delete'
+```
--- .github/preview-index/src/update.js
+++ .github/preview-index/src/update.js
@@ -16,35 +16,29 @@
  ************************************************************************* */
 
 const fetch = require('node-fetch-commonjs');
-const crypto = require('crypto');
 const msal = require('@azure/msal-node');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 require('dotenv').config();
+
 const SP_CLIENT_ID = process.env.SP_CLIENT_ID;
 const SP_TENANT_ID = process.env.SP_TENANT_ID;
-const SP_CERT_PASSWORD = process.env.SP_CERT_PASSWORD;
-const SP_CERT_THUMB_PRINT = process.env.SP_CERT_THUMB_PRINT;
-const SP_CERT_CONTENT = process.env.SP_CERT_CONTENT;
+const SP_CLIENT_SECRET = process.env.SP_CLIENT_SECRET;
 const SP_DRIVE_ID = process.env.SP_DRIVE_ID;
+const EDS_ADMIN_KEY = process.env.EDS_ADMIN_KEY;
+const EDS_ACCESS_KEY = process.env.EDS_ACCESS_KEY;
+const CONSUMER = process.env.CONSUMER;
 const PREVIEW_INDEX_FILE = process.env.PREVIEW_INDEX_FILE;
-const PREVIEW_RESOURCES_FOLDER = process.env.PREVIEW_INDEX_FILE;
+const PREVIEW_RESOURCES_FOLDER = process.env.PREVIEW_RESOURCES_FOLDER; 
 
+const PREVIEW_STATUS_URL = `https://admin.hlx.page/status/adobecom/${CONSUMER}/main/*`;
+const PREVIEW_BASE_URL = `https://main--${CONSUMER}--adobecom.hlx.page`;
 const GRAPH_BASE_URL = `https://graph.microsoft.com/v1.0`;
 const SHEET_RAW_INDEX = 'raw_index';
 const TABLE_NAME = 'Table1';
 const FETCH_RETRY = 10;
 let accessToken;
-
-const parseCert = (content, password) => crypto.createPrivateKey({
-  key: content,
-  passphrase: password,
-  format: 'pem'
-}).export({
-  format: 'pem',
-  type: 'pkcs8'
-});
 
 const decodeToObject = (base64String) => {
   try {
@@ -69,15 +63,11 @@ const getAccessToken = async () => {
   if (!accessToken || isTokenExpired(accessToken)) {
     console.log('fetching access token...')
     const authConfig = {
-        auth: {
-            clientId: SP_CLIENT_ID,
-            authority: `https://login.microsoftonline.com/${SP_TENANT_ID}`,
-            knownAuthorities: ['login.microsoftonline.com'],
-            clientCertificate: {
-                privateKey: parseCert(SP_CERT_CONTENT, SP_CERT_PASSWORD),
-                thumbprint: SP_CERT_THUMB_PRINT,
-            }
-        }
+      auth: {
+          clientId: SP_CLIENT_ID,
+          authority: `https://login.microsoftonline.com/${SP_TENANT_ID}`,
+          clientSecret: encodeURIComponent(SP_CLIENT_SECRET)
+      }
     }
     const authClient = new msal.ConfidentialClientApplication(authConfig);
     const request = {
@@ -85,14 +75,18 @@ const getAccessToken = async () => {
     };
     const tokens = await authClient.acquireTokenByClientCredential(request);
     accessToken = tokens.accessToken;
-    console.log('token fetched.')
+    console.log('token fetched.');
   }
   return accessToken;
 }
 
 const getResourceIndexData = async (resource) => {
-  const url = `https://main--milo--adobecom.hlx.page${resource.path}`;
-  const response = await fetch(url);
+  let headers = {
+    Authorization: `token ${EDS_ACCESS_KEY}`,
+    'Content-Type': 'application/json'
+  };
+  const url = `${PREVIEW_BASE_URL}${resource.path}`;
+  const response = await fetch(url, {headers});
   if (response?.status !== 200) {
     console.log('Failed to fetch card: ' + url);
     return;
@@ -147,9 +141,9 @@ const getItemId = async (indexPath) => {
 };
 
 const getPreviewResources = async (folder, parseIndexFc) => {
-  const PREVIEW_STATUS_URL = 'https://admin.hlx.page/status/adobecom/milo/main/*';
   const data = {"select": ["preview"], "paths": [folder]};
-  let headers = {...await defaultHeaders(),
+  let headers = {
+    Authorization: `token ${EDS_ADMIN_KEY}`,
     'Content-Type': 'application/json'
   };
   const response = await fetch(PREVIEW_STATUS_URL, {
@@ -162,16 +156,19 @@ const getPreviewResources = async (folder, parseIndexFc) => {
     return;
   }
   const job = await response.json();
-  const jobDetailsURL = `${job.links.self}/details`;
+  let jobDetailsURL = `${job.links.self}/details`;
+  jobDetailsURL = 'https://admin.hlx.page/job/adobecom/cc/main/status/job-2024-07-29t17-33-30-392z/details';
 
   const retryFetch = async (resolve, attempt = 1) => {
-    const response = await fetch(jobDetailsURL, { headers: await defaultHeaders() });
+    const response = await fetch(jobDetailsURL, {
+      headers
+    });
     const data = await response.json();
     if (data?.state === 'stopped') {
         resolve(data?.data);
     } else if (attempt < FETCH_RETRY) {
         console.log(`Attempt ${attempt}: Job state is '${data?.state}'. Checking again in 1 second...`);
-        setTimeout(() => retryFetch(resolve, attempt + 1), 1000); // Wait 1 second before the next check
+        setTimeout(() => retryFetch(resolve, attempt + 1), 5000); // Wait 1 second before the next check
     } else {
         console.log("Maximum attempts reached. Stopping the fetching.");
         resolve(undefined);
@@ -193,7 +190,8 @@ const getPreviewResources = async (folder, parseIndexFc) => {
 
   const indexData = jsonPromises
     .filter((p) => p.status === 'fulfilled')
-    .map((p) => p.value);
+    .map((p) => p.value)
+    .filter((value) => !!value);
   console.log(`fetched ${indexData?.length} previewed resources`);
   return indexData;
 }
@@ -222,10 +220,11 @@ const validateConfig = () => {
   const config = {
     SP_CLIENT_ID: SP_CLIENT_ID,
     SP_TENANT_ID: SP_TENANT_ID,
-    SP_CERT_PASSWORD: SP_CERT_PASSWORD,
-    SP_CERT_THUMB_PRINT: SP_CERT_THUMB_PRINT,
-    SP_CERT_CONTENT: SP_CERT_CONTENT,
+    SP_CLIENT_SECRET: SP_CLIENT_SECRET,
     SP_DRIVE_ID: SP_DRIVE_ID,
+    EDS_ADMIN_KEY: EDS_ADMIN_KEY,
+    EDS_ACCESS_KEY: EDS_ACCESS_KEY,
+    CONSUMER: CONSUMER,
     PREVIEW_INDEX_FILE: PREVIEW_INDEX_FILE,
     PREVIEW_RESOURCES_FOLDER: PREVIEW_RESOURCES_FOLDER,
   };
@@ -248,7 +247,8 @@ const reindex = async (indexPath, folder) => {
   }
 
   const indexData = await getPreviewResources(folder, getResourceIndexData);
-  if (!indexData) {
+  // todo think if we want to delete rows in index table in case no cards found..
+  if (!indexData?.length) {
     console.log('No index data found.');
     return;
   }
