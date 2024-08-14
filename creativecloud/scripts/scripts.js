99e95b98628fd8b2ffcabf9ca13abb36264a3a98
Aishwarya Mathuria <mathuria@adobe.com>
2024-08-14 14:58:24 +0530

    Update scripts.js

--- creativecloud/scripts/scripts.js
+++ creativecloud/scripts/scripts.js
@@ -207,7 +207,7 @@ decorateArea();
     a.addEventListener('click', () => {
       setTimeout(() => {
         window.location = `https://${rlink}`;
-      }, 100)
+      }, 3000)
     });
   });
   loadLana({ clientId: 'cc' });
