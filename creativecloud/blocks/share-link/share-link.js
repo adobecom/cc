472c529aff3d0675a5334c1074d6eb6e76dd6ba4
Drashti Modasara <dmodasara@adobe.com>
2024-08-25 13:35:26 +0530

    new block

--- creativecloud/blocks/share-link/share-link.js
+++ creativecloud/blocks/share-link/share-link.js
@@ -1,27 +1,18 @@
-import { createTag } from '../../scripts/utils.js';
+import { getLibs } from '../../scripts/utils.js';
+const miloLibs = getLibs('/libs');
+const { decorateButtons } = await import(`${miloLibs}/utils/decorate.js`);
 
 export default async function init(el) {
-  const fg = el.querySelector('div:first-child');
-  const link = el.querySelector('a').innerHTML;
+  const btnLink = el.querySelector('a').href;
 
-  for (const child of fg.children) {
-    child.classList.toggle('btn-area', !!child.querySelector('picture'));
-    child.classList.toggle('text', !child.querySelector('picture'));
-  }
-  fg.classList.add('foreground');
-
-  const btnEl = document.querySelector(".btn-area");
-  const wcWebSharing = createTag('wc-web-sharing', { class: 'web-sharing', link });
-  btnEl.appendChild(wcWebSharing);
-
-  const shadow = wcWebSharing.attachShadow({ mode: "open" });
-  const pic = btnEl.querySelector('picture');
-  if (pic) shadow.appendChild(pic);
-
-  wcWebSharing.addEventListener('click', async () => {
+   decorateButtons(el.querySelector('div > div'), 'button-l');
+   const aTag = el.querySelector('a');
+   aTag.href = '';
+  
+  aTag.addEventListener('click', async () => {
     if (navigator.share) {
       try {
-        await navigator.share({ title: 'send', url: link });
+        await navigator.share({ title: 'send', url: btnLink });
       } catch (err) {
         window.lana.log(`Error using navigator object: ${err}`, { tags: 'errorType=error,module=share-link' });
       }
