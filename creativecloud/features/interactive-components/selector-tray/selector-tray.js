679197334e8dc87a9fb42ecf00739aa8cc212210
mathuria <mathuria@adobe.com>
2024-09-02 11:43:24 +0530

    prevent default

--- creativecloud/features/interactive-components/selector-tray/selector-tray.js
+++ creativecloud/features/interactive-components/selector-tray/selector-tray.js
@@ -34,14 +34,12 @@ function createSelectorThumbnail(pic, pathId, displayImg) {
 function attachThumbnailEvents(a, data, layer) {
   ['mouseover', 'touchstart', 'focus', 'keyup'].forEach((event) => {
     a.addEventListener(event, (e) => {
-      e.preventDefault();
       const selected = e.target.closest('.tray-items')?.querySelectorAll('.thumbnail-selected');
       [...selected].forEach((s) => s.classList.remove('thumbnail-selected'));
     });
   });
   ['mouseover'].forEach((event) => {
     a.addEventListener(event, async (e) => {
-      e.preventDefault();
       const curra = e.target.nodeName === 'A' ? e.target : e.target.closest('a');
       const selected = e.target.closest('.tray-items')?.querySelectorAll('.thumbnail-selected');
       [...selected].forEach((s) => s.classList.remove('thumbnail-selected'));
