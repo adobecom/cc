e997b4156c4e31e265b19780cf8dfaeab66704f3
Ilyas Türkben <isturkben@gmail.com>
2024-09-04 15:33:37 +0200

    Update utils.js

--- creativecloud/scripts/utils.js
+++ creativecloud/scripts/utils.js
@@ -24,7 +24,7 @@ export const [setLibs, getLibs] = (() => {
   let libs;
   return [
     (prodLibs, force = false) => {
-      libs = 'https://mas-xlg-hackathon--mas--adobecom.hlx.live/libs';
+      libs = 'https://mas-xlg-hackathon--milo--adobecom.hlx.live/libs';
       return libs;
       if (force) {
         libs = prodLibs;
