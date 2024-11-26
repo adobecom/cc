4e9b9a5cf202705ef770bccf906eb8cc0944f095
Akansha Arora <>
2024-11-26 19:13:06 +0530

    Suggested Changes

--- creativecloud/blocks/privacy-button/privacy-button.js
+++ creativecloud/blocks/privacy-button/privacy-button.js
@@ -17,7 +17,6 @@ function handleUrlVariant(button, urls, message) {
         injectTrackingImage(trimmedUrl);
       }
     });
-    // Create a new element for the success message
     button.parentNode.replaceChild(createTag('span', { class: 'privacy-button-message' }, message), button);
   });
 }
@@ -28,9 +27,8 @@ export default async function init(el) {
   const config = {};
   el.querySelectorAll(':scope > div').forEach((row) => {
     const [key, value] = [...row.children];
-    if (key && value) {
-      config[key.textContent.trim().toLowerCase()] = value.textContent.trim();
-    }
+    if (!key || !value) return;
+    config[key.textContent.trim().toLowerCase()] = value.textContent.trim();
   });
 
   const button = createTag('button', {
