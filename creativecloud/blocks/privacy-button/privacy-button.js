6f2f15792e9c92a6c81c161617611491dd35e88d
Akansha Arora <>
2024-11-26 14:05:24 +0530

    Privacy-button

---
+++ creativecloud/blocks/privacy-button/privacy-button.js
@@ -0,0 +1,48 @@
+import { getLibs, createTag } from '../../scripts/utils.js';
+
+function injectTrackingImage(url) {
+  const img = createTag('img', {
+    src: url,
+    class: 'privacy-tracking-image',
+    'aria-hidden': 'true',
+  });
+  document.body.appendChild(img);
+}
+
+function handleUrlVariant(button, urls, message) {
+  button.addEventListener('click', () => {
+    urls.split(',').forEach((url) => {
+      const trimmedUrl = url.trim();
+      if (trimmedUrl.match('^http') !== null) {
+        injectTrackingImage(trimmedUrl);
+      }
+    });
+    // Create a new element for the success message
+    button.parentNode.replaceChild(createTag('span', { class: 'privacy-button-message' }, message), button);
+  });
+}
+
+export default async function init(el) {
+  const miloLibs = getLibs('/libs');
+  const { decorateButtons } = await import(`${miloLibs}/utils/decorate.js`);
+  const config = {};
+  el.querySelectorAll(':scope > div').forEach((row) => {
+    const [key, value] = [...row.children];
+    if (key && value) {
+      config[key.textContent.trim().toLowerCase()] = value.textContent.trim();
+    }
+  });
+
+  const button = createTag('button', {
+    class: 'con-button blue',
+    type: 'button',
+  }, config['button-label']);
+
+  if (config['button-label'] && config['confirmation-message'] && config.url) {
+    handleUrlVariant(button, config.url, config['confirmation-message']);
+  }
+
+  el.innerHTML = '';
+  el.append(button);
+  decorateButtons(el);
+}
