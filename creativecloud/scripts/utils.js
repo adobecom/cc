11ddfddd4cbbc8a653a206818b343cbef89a517b
Aishwarya Mathuria <mathuria@adobe.com>
2024-12-10 14:47:10 +0530

    Update utils.js

--- creativecloud/scripts/utils.js
+++ creativecloud/scripts/utils.js
@@ -185,8 +185,8 @@ export const [setLibs, getLibs] = (() => {
 
 const miloLibs = setLibs('/libs');
 
-const { createTag, localizeLink, getConfig, loadStyle, createIntersectionObserver } = await import(`${miloLibs}/utils/utils.js`);
-export { createTag, loadStyle, localizeLink, createIntersectionObserver, getConfig };
+const { createTag, localizeLink, getConfig, loadStyle, loadLink, loadScript, createIntersectionObserver } = await import(`${miloLibs}/utils/utils.js`);
+export { createTag, loadStyle, loadLink, loadScript, localizeLink, createIntersectionObserver, getConfig };
 
 function defineDeviceByScreenSize() {
   const DESKTOP_SIZE = 1200;
