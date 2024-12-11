f1d034203aef7e8fa09eb424fc10f3bee7e9804c
Aishwarya Mathuria <mathuria@adobe.com>
2024-12-11 13:16:53 +0530

    Revert "MWPW-163234 | Parallel load stack for Unity enabled page performance …" (#490)
    This reverts commit 1bc9346df384cd51c13db41b7d6bdfccaf99568d.

--- creativecloud/blocks/unity/unity.js
+++ creativecloud/blocks/unity/unity.js
@@ -1,25 +1,4 @@
-import { loadLink, loadScript } from '../../scripts/utils.js';
-
-async function priorityLoad(parr) {
-  const promiseArr = [];
-  parr.forEach((p) => {
-    if (p.endsWith('.js')) {
-      const pr = loadScript(p, 'module', { mode: 'async' });
-      promiseArr.push(pr);
-    } else if (p.endsWith('.css')) {
-      const pr = new Promise((res) => { loadLink(p, { rel: 'stylesheet', callback: res }); });
-      promiseArr.push(pr);
-    } else if (p.endsWith('.json')) {
-      const pr = new Promise((res) => { loadLink(p, { as: 'fetch', crossorigin: 'anonymous', rel: 'preload', callback: res }); });
-      promiseArr.push(pr);
-    } else {
-      promiseArr.push(fetch(p));
-    }
-  });
-  try {
-    await Promise.all(promiseArr);
-  } catch (e) { /* do not error out if call fails */ }
-}
+import { loadStyle } from '../../scripts/utils.js';
 
 function getUnityLibs(prodLibs = '/unitylibs') {
   const { hostname } = window.location;
@@ -35,20 +14,10 @@ function getUnityLibs(prodLibs = '/unitylibs') {
 
 export default async function init(el) {
   const unitylibs = getUnityLibs();
-  const promiseArr = [
-    `${unitylibs}/core/styles/styles.css`,
-    `${unitylibs}/core/workflow/workflow.js`,
-    `${unitylibs}/scripts/utils.js`,
-    `${unitylibs}/core/workflow/workflow-photoshop/workflow-photoshop.js`,
-    `${unitylibs}/core/workflow/workflow-photoshop/workflow-photoshop.css`,
-    `${unitylibs}/core/steps/upload-btn.js`,
-    `${unitylibs}/core/steps/app-connector.js`,
-  ];
-  await priorityLoad(promiseArr);
-  const promiseSvgArr = [];
-  const allSvgs = el.querySelectorAll('img[src*=svg]');
-  [...allSvgs].slice(0, 6).forEach((s) => promiseSvgArr.push(s.src));
-  await priorityLoad(promiseSvgArr);
+  const stylePromise = new Promise((resolve) => {
+    loadStyle(`${unitylibs}/core/styles/styles.css`, resolve);
+  });
+  await stylePromise;
   const { default: wfinit } = await import(`${unitylibs}/core/workflow/workflow.js`);
   await wfinit(el, 'cc', unitylibs);
 }
