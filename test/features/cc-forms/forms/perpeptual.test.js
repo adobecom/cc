d29345ba34b167be90c447224bf54c229f0d9708
mathuria <mathuria@adobe.com>
2025-02-01 00:20:32 +0530

    Unit tests

--- test/features/cc-forms/forms/perpeptual.test.js
+++ test/features/cc-forms/forms/perpeptual.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-promise-executor-return */
 import sinon from 'sinon';
+import { expect } from '@esm-bundle/chai';
 import { readFile } from '@web/test-runner-commands';
 import { setLibs, getLibs } from '../../../../creativecloud/scripts/utils.js';
-import { expect } from '@esm-bundle/chai';
 
 const miloLibs = '/libs';
 setLibs(miloLibs);
@@ -47,20 +48,21 @@ describe('Perpeptual Form', async () => {
     };
     fetchStub.callsFake((url) => {
       if (url.includes('countries')) {
-        return Promise.resolve(new Response(JSON.stringify({"data":{"countryList":{"items":[{"_path":"/content/dam/acom/country/us/en/US","id":"US","title":"United States","value":"US","isTop":false},{"_path":"/content/dam/acom/country/us/en/IN","id":"IN","title":"India","value":"IN","isTop":false}]}}}), { status: 200 }));
+        return Promise.resolve(new Response(`${''} + {'data':{'countryList':{'items':[{'_path':'/content/dam/acom/country/us/en/US','id':'US','title':'United States','value':'US','isTop':false},{'_path':'/content/dam/acom/country/us/en/IN','id':'IN','title':'India','value':'IN','isTop':false}]}}}`, { status: 200 }));
       }
       if (url.includes('jobtitle')) {
-        return Promise.resolve(new Response(JSON.stringify({"data":{"formfieldvaluesList":{"items":[{"id":0,"title":"Vice President","value":"VICE_PRESIDENT"},{"id":0,"title":"Web Manager","value":"WEB_MANAGER"}]}}}), { status: 200 }));
+        return Promise.resolve(new Response(`${''} + {'data':{'formfieldvaluesList':{'items':[{'id':0,'title':'Vice President','value':'VICE_PRESIDENT'},{'id':0,'title':'Web Manager','value':'WEB_MANAGER'}]}}}`, { status: 200 }));
       }
       if (url.includes('listskuproductversiondetails')) {
-        return Promise.resolve(new Response(JSON.stringify({"data":{"productskuList":{"items":[{"emailtemplate":"tdrc_captivate","imscontextid":"pt_captivate","productname":"captivate","productcode":"captivate","requestcode":null,"version":[{"title":"2023","productlabel":"Captivate 2023","language_os":[{"downloadmethod":"1","filesize":"1555494775","fileurl":"https://localhost","title":"Brazilian Portuguese-Windows","language":"Brazilian Portuguese","platform":"Windows","sku":"skunum","active":"active"}]},{"title":"2019","productlabel":"Captivate 2019","language_os":[{"downloadmethod":"1","filesize":"3185352464","fileurl":"https://localhost","title":"Brazilian Portuguese-Windows-64 bit","language":"Brazilian Portuguese","platform":"Windows 64bit","sku":"skunum","active":"active"}]}]}]}}}), { status: 500 }));
+        return Promise.resolve(new Response(`${''} + {'data':{'productskuList':{'items':[{'emailtemplate':'tdrc_captivate','imscontextid':'pt_captivate','productname':'captivate','productcode':'captivate','requestcode':null,'version':[{'title':'2023','productlabel':'Captivate 2023','language_os':[{'downloadmethod':'1','filesize':'1555494775','fileurl':'https://localhost','title':'Brazilian Portuguese-Windows','language':'Brazilian Portuguese','platform':'Windows','sku':'skunum','active':'active'}]},{'title':'2019','productlabel':'Captivate 2019','language_os':[{'downloadmethod':'1','filesize':'3185352464','fileurl':'https://localhost','title':'Brazilian Portuguese-Windows-64 bit','language':'Brazilian Portuguese','platform':'Windows 64bit','sku':'skunum','active':'active'}]}]}]}}}`, { status: 200 }));
       }
       if (url.includes('listskuproductandversions')) {
-        return Promise.resolve(new Response(JSON.stringify({"data": {"productskuList": {"items": [{"emailtemplate": "tdrc_captivate","imscontextid": "pt_captivate","productname": "captivate","productcode": "captivate","requestcode": null,"version": [{"title": "2023"},{"title": "2019"}]}]}}}), { status: 500 }));
+        return Promise.resolve(new Response(`${''} + {'data': {'productskuList': {'items': [{'emailtemplate': 'tdrc_captivate','imscontextid': 'pt_captivate','productname': 'captivate','productcode': 'captivate','requestcode': null,'version': [{'title': '2023'},{'title': '2019'}]}]}}}`, { status: 500 }));
       }
       if (url.includes('existinguser')) {
-        return Promise.resolve(new Response(JSON.stringify({"data":{"formfieldvaluesList":{"items":[{"id":4,"title":"Not Using Adobe Captivate","value":"notusingadobecaptivate"},{"id":3,"title":"Older versions","value":"olderversions"}]}}}), { status: 500 }));
+        return Promise.resolve(new Response(`${''} + {'data':{'formfieldvaluesList':{'items':[{'id':4,'title':'Not Using Adobe Captivate','value':'notusingadobecaptivate'},{'id':3,'title':'Older versions','value':'olderversions'}]}}}`, { status: 500 }));
       }
+      return Promise.resolve();
     });
     await init(el);
   });
