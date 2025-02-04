4c8fc01d7c2d72c4b955243bbf5670ca6d3efad0
Aishwarya Mathuria <mathuria@adobe.com>
2025-02-04 14:17:45 +0530

    MWPW-165982 | Adding more unit tests for perpeptual coverage (#604)
    Adding more unit tests for perpeptual coverage
    Resolves: MWPW-165982
    
    Test URLs:
    
    Before: https://main--cc--adobecom.hlx.live/drafts/mathuria/dalpforms/perpeptual?martech=off&georouting=off
    After: https://forms-unittest--cc--adobecom.hlx.live/drafts/mathuria/dalpforms/perpeptual1?martech=off&georouting=off

--- test/blocks/cc-forms/cc-forms.test.js
+++ test/blocks/cc-forms/cc-forms.test.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-promise-executor-return */
+/* eslint-disable object-curly-newline */
+/* eslint-disable comma-spacing */
+/* eslint-disable key-spacing */
+/* eslint-disable quote-props */
+/* eslint-disable object-curly-spacing */
 import sinon from 'sinon';
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
@@ -30,6 +35,9 @@ window.adobeid = {
   locale: 'en',
 };
 
+window.digitalData = {};
+window.alloy_all = { 'data': { '_adobe_corpnew' : { 'digitalData' : {}}}};
+
 function delay(ms) {
   return new Promise((res) => { setTimeout(() => { res(); }, ms); });
 }
@@ -37,30 +45,32 @@ function delay(ms) {
 const { default: init } = await import('../../../creativecloud/blocks/cc-forms/cc-forms.js');
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
-describe('Perpeptual Form', () => {
+describe('CC Form', () => {
   const fetchStub = sinon.stub(window, 'fetch');
   before(async () => {
     const el = document.querySelector('.cc-forms');
     fetchStub.callsFake((url) => {
+      let payload = {};
       if (url.includes('countries')) {
-        return Promise.resolve(new Response(`${''} + {'data':{'countryList':{'items':[{'_path':'/content/dam/acom/country/us/en/US','id':'US','title':'United States','value':'US','isTop':false},{'_path':'/content/dam/acom/country/us/en/IN','id':'IN','title':'India','value':'IN','isTop':false}]}}}`, { status: 200 }));
+        payload = {'data':{'countryList':{'items':[{'_path':'/content/dam/acom/country/us/en/US','id':'US','title':'United States','value':'US','isTop':false},{'_path':'/content/dam/acom/country/us/en/IN','id':'IN','title':'India','value':'IN','isTop':false}]}}};
+      } else if (url.includes('jobtitle')) {
+        payload = {'data':{'formfieldvaluesList':{'items':[{'id':0,'title':'Vice President','value':'VICE_PRESIDENT'},{'id':0,'title':'Web Manager','value':'WEB_MANAGER'}]}}};
+      } else if (url.includes('listskuproductversiondetails')) {
+        payload = {'data':{'productskuList':{'items':[{'emailtemplate':'tdrc_captivate','imscontextid':'pt_captivate','productname':'captivate','productcode':'captivate','requestcode':null,'version':[{'title':'2023','productlabel':'Captivate 2023','language_os':[{'downloadmethod':'1','filesize':'1555494775','fileurl':'https://localhost','title':'Brazilian Portuguese-Windows','language':'Brazilian Portuguese','platform':'Windows','sku':'skunum','active':'active'}]},{'title':'2019','productlabel':'Captivate 2019','language_os':[{'downloadmethod':'1','filesize':'3185352464','fileurl':'https://localhost','title':'Brazilian Portuguese-Windows-64 bit','language':'Brazilian Portuguese','platform':'Windows 64bit','sku':'skunum','active':'active'}]}]}]}}};
+      } else if (url.includes('listskuproductandversions')) {
+        payload = {'data': {'productskuList': {'items': [{'emailtemplate': 'tdrc_captivate','imscontextid': 'pt_captivate','productname': 'captivate','productcode': 'captivate','requestcode': null,'version': [{'title': '2023'},{'title': '2019'}]}]}}};
+      } else if (url.includes('existinguser')) {
+        payload = {'data':{'formfieldvaluesList':{'items':[{'id':4,'title':'Not Using Adobe Captivate','value':'notusingadobecaptivate'},{'id':3,'title':'Older versions','value':'olderversions'}]}}};
+      } else if (url.includes('marketing_common_service')) {
+        // pass
+      } else if (url.includes('connect_trial_creation_service')) {
+        // pass
       }
-      if (url.includes('jobtitle')) {
-        return Promise.resolve(new Response(`${''} + {'data':{'formfieldvaluesList':{'items':[{'id':0,'title':'Vice President','value':'VICE_PRESIDENT'},{'id':0,'title':'Web Manager','value':'WEB_MANAGER'}]}}}`, { status: 200 }));
-      }
-      if (url.includes('listskuproductversiondetails')) {
-        return Promise.resolve(new Response(`${''} + {'data':{'productskuList':{'items':[{'emailtemplate':'tdrc_captivate','imscontextid':'pt_captivate','productname':'captivate','productcode':'captivate','requestcode':null,'version':[{'title':'2023','productlabel':'Captivate 2023','language_os':[{'downloadmethod':'1','filesize':'1555494775','fileurl':'https://localhost','title':'Brazilian Portuguese-Windows','language':'Brazilian Portuguese','platform':'Windows','sku':'skunum','active':'active'}]},{'title':'2019','productlabel':'Captivate 2019','language_os':[{'downloadmethod':'1','filesize':'3185352464','fileurl':'https://localhost','title':'Brazilian Portuguese-Windows-64 bit','language':'Brazilian Portuguese','platform':'Windows 64bit','sku':'skunum','active':'active'}]}]}]}}}`, { status: 200 }));
-      }
-      if (url.includes('listskuproductandversions')) {
-        return Promise.resolve(new Response(`${''} + {'data': {'productskuList': {'items': [{'emailtemplate': 'tdrc_captivate','imscontextid': 'pt_captivate','productname': 'captivate','productcode': 'captivate','requestcode': null,'version': [{'title': '2023'},{'title': '2019'}]}]}}}`, { status: 500 }));
-      }
-      if (url.includes('existinguser')) {
-        return Promise.resolve(new Response(`${''} + {'data':{'formfieldvaluesList':{'items':[{'id':4,'title':'Not Using Adobe Captivate','value':'notusingadobecaptivate'},{'id':3,'title':'Older versions','value':'olderversions'}]}}}`, { status: 500 }));
-      }
-      if (url.includes('listallregionsbypath')) {
-        return Promise.resolve(new Response(`${''} + {"data":{"countryByPath":{"item":{"child":[{"_path":"/content/dam/acom/child_country/en/JP_IWATEKEN","id":"JP_IWATEKEN","title":"Iwate-ken","value":"IWATEKEN"},{"_path":"/content/dam/acom/child_country/en/JP_ISHIKAWA","id":"JP_ISHIKAWA","title":"Ishikawa-ken","value":"ISHIKAWA"}]}}}}`, { status: 500 }));
-      }
-      return Promise.resolve();
+      return Promise.resolve({
+        json: async () => payload,
+        status: 200,
+        ok: true,
+      });
     });
     await init(el);
     await delay(500);
@@ -76,5 +86,17 @@ describe('Perpeptual Form', () => {
 
   it('Text fields should populate', () => {
     expect(document.querySelector('input')).to.exist;
+  });
+
+  it('Submit check', () => {
+    document.body.querySelector('#orgname').value = 'val';
+    document.body.querySelector('#postalcode').value = 'val';
+    document.body.querySelector('#phonenumber').value = 'val';
+    document.body.querySelector('#country').selectedIndex = 1;
+    document.body.querySelector('#state').selectedIndex = 1;
+    document.body.querySelector('#jobtitle').selectedIndex = 1;
+    document.body.querySelector('#usertype').selectedIndex = 1;
+    document.body.querySelector('#productsku').selectedIndex = 1;
+    document.body.querySelector('.cc-form-component.submit').dispatchEvent(new Event('click'));
   });
 });
