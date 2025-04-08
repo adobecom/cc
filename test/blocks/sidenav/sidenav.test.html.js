d4e7535fe8d8b803829950bcd24d895a276669e4
hkuraware <hkurware@adobe.com>
2025-04-08 13:10:23 +0530

    test commit

--- test/blocks/sidenav/sidenav.test.html.js
+++ test/blocks/sidenav/sidenav.test.html.js
@@ -49,6 +49,7 @@ runTests(async () => {
 
     it('does create nice categories default sidenav', async () => {
       const result = await testCategorySidenav('.categories', 24, 18);
+      console.log(result);
       expect(result).to.be.undefined;
     });
 
