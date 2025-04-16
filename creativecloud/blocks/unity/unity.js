dec7a4bc9f534298cfda903e82439f45cb8db61d
himani-kuraware <hkuraware@adobe.com>
2025-04-16 15:08:11 +0530

    fix(MWPW-164079): fixed Ssrf

--- creativecloud/blocks/unity/unity.js
+++ creativecloud/blocks/unity/unity.js
@@ -1,26 +1,5 @@
 import { loadLink, loadScript } from '../../scripts/utils.js';
 
-async function priorityLoad(parr) {
-  const promiseArr = [];
-  parr.forEach((p) => {
-    if (p.endsWith('.js')) {
-      const pr = loadScript(p, 'module', { mode: 'async' });
-      promiseArr.push(pr);
-    } else if (p.endsWith('.css')) {
-      const pr = new Promise((res) => { loadLink(p, { rel: 'stylesheet', callback: res }); });
-      promiseArr.push(pr);
-    } else if (p.endsWith('.json')) {
-      const pr = new Promise((res) => { loadLink(p, { as: 'fetch', crossorigin: 'anonymous', rel: 'preload', callback: res }); });
-      promiseArr.push(pr);
-    } else {
-      promiseArr.push(fetch(p));
-    }
-  });
-  try {
-    await Promise.all(promiseArr);
-  } catch (e) { /* do not error out if call fails */ }
-}
-
 function getUnityLibs(prodLibs = '/unitylibs') {
   const { hostname } = window.location;
   if (!hostname.includes('hlx.page')
@@ -34,6 +13,28 @@ function getUnityLibs(prodLibs = '/unitylibs') {
   const env = hostname.includes('.hlx.') ? 'hlx' : 'aem';
   if (branch.indexOf('--') > -1) return `https://${branch}.${env}.live/unitylibs`;
   return `https://${branch}--unity--adobecom.${env}.live/unitylibs`;
+}
+
+async function priorityLoad(parr) {
+  const promiseArr = [];
+  const allowedBaseUrl = getUnityLibs();
+  parr.forEach((p) => {
+    if (p.endsWith('.js')) {
+      const pr = loadScript(p, 'module', { mode: 'async' });
+      promiseArr.push(pr);
+    } else if (p.endsWith('.css')) {
+      const pr = new Promise((res) => { loadLink(p, { rel: 'stylesheet', callback: res }); });
+      promiseArr.push(pr);
+    } else if (p.endsWith('.json')) {
+      const pr = new Promise((res) => { loadLink(p, { as: 'fetch', crossorigin: 'anonymous', rel: 'preload', callback: res }); });
+      promiseArr.push(pr);
+    } else if (p.startsWith(allowedBaseUrl)) {
+      promiseArr.push(fetch(p));
+    }
+  });
+  try {
+    await Promise.all(promiseArr);
+  } catch (e) { /* do not error out if call fails */ }
 }
 
 export default async function init(el) {
