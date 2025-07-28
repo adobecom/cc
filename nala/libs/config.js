fdcf222aa0d2f4403cba58318acf07b7c97b6c90
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2025-07-28 12:21:28 +0530

    fix config.js projects

--- nala/libs/config.js
+++ nala/libs/config.js
@@ -2,8 +2,8 @@
 const PROJECT = 'cc';
 const ORG = 'adobecom';
 const BRANCHES = { main: 'main', stage: 'stage' };
-const MAIN_BRANCH_LIVE_URL = `https://${BRANCHES.main}--${PROJECT}--${ORG}.hlx.live`;
-const STAGE_BRANCH_URL = `https://${BRANCHES.stage}--${PROJECT}--${ORG}.hlx.live`;
+const MAIN_BRANCH_LIVE_URL = `https://${BRANCHES.main}--${PROJECT}--${ORG}.aem.live`;
+const STAGE_BRANCH_URL = `https://${BRANCHES.stage}--${PROJECT}--${ORG}.aem.live`;
 const BASE_URLS = { local: 'http://localhost:3000', stage: STAGE_BRANCH_URL, main: MAIN_BRANCH_LIVE_URL };
 
 module.exports = {
