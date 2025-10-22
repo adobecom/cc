4fdeccfcd565134f5b28b134b97800c7aea3d96b
Nishant Kumar Thakur <nishant.thakur.ece@gmail.com>
2025-10-22 12:56:32 +0530

    [MAX25][FF][Showcase] Incorporated fix for bleeding by Narcis

--- creativecloud/blocks/firefly-model-showcase/firefly-model-showcase.js
+++ creativecloud/blocks/firefly-model-showcase/firefly-model-showcase.js
@@ -67,6 +67,7 @@ function buildGalleryOutline(parentElem) {
 export default async function init(el) {
   const miloLibs = getLibs('/libs');
   const { decorateButtons } = await import(`${miloLibs}/utils/decorate.js`);
+
   const showcaseContentElem = el.querySelector(':scope > div');
   // Add class to container for styling
   showcaseContentElem.classList.add('firefly-model-showcase-content');
