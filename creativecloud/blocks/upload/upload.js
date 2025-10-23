04fd2202dc8db9c3426894991dcede49d91cca79
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2025-10-23 13:12:07 -0700

    Update upload.js

--- creativecloud/blocks/upload/upload.js
+++ creativecloud/blocks/upload/upload.js
@@ -18,7 +18,7 @@ function decorateUploadEls(para) {
     {
       type: 'button',
       class: 'con-button blue action-button button-xl',
-      'daa-ll': 'Upload your photo|UnityWidget'
+      'daa-ll': 'Upload your photo UnityWidget',
     },
     para.innerHTML,
   );
