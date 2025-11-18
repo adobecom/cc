22f3211ff276c706002808fe4bbe1a2883229083
sigadamramesh <130433161+sigadamvenkata@users.noreply.github.com>
2025-11-18 10:42:15 +0530

    fixing the issues with page
    fixing the issues with page

--- nala/blocks/inlinevideo/inlinevideo.spec.js
+++ nala/blocks/inlinevideo/inlinevideo.spec.js
@@ -4,13 +4,13 @@ module.exports = {
     {
       tcid: '0',
       name: '@inlinevideo-play',
-      path: '/products/illustrator?georouting=off&mep=off',
+      path: '/products/premiere?georouting=off&mep=off',
       tags: '@cc @cc-inlinevideo @cc-inlinevideoplay',
     },
     {
       tcid: '1',
       name: '@inlinevideo-pause',
-      path: '/products/illustrator?georouting=off&mep=off',
+      path: '/products/premiere?georouting=off&mep=off',
       tags: '@cc @cc-inlinevideo @cc-inlinevideopause',
     },
   ],
