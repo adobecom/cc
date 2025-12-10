7a36a9d32bb531cdc7889e8dd0e2cdd5999308e9
Vivian A Goodrich <vgoodric@adobe.com>
2025-12-10 13:08:49 -0700

    feat: added download URL pattern to CONFIG for Adobe links

--- creativecloud/scripts/utils.js
+++ creativecloud/scripts/utils.js
@@ -428,6 +428,7 @@ const CONFIG = {
     /www\.adobe\.com\/(\w\w(_\w\w)?\/)?go(\/.*)?/,
     /www\.adobe\.com\/(\w\w(_\w\w)?\/)?learn(\/.*)?/,
     /www\.adobe\.com\/(\w\w(_\w\w)?\/)?benefits(\/.*)?/,
+    /www\.adobe\.com\/(\w\w(_\w\w)?\/)?download(\/.*)?/,
   ],
   brandConciergeAA: 'cc:app-reco',
 };
