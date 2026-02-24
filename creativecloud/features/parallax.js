4bc1683426108cad319ba38b10e05d5b1f6f04ab
himani-kuraware <hkuraware@adobe.com>
2026-02-24 18:58:21 +0530

    Updated the logic for button

--- creativecloud/features/parallax.js
+++ creativecloud/features/parallax.js
@@ -21,9 +21,13 @@ function throttle(cb, delay, { trailing = false } = {}) {
   };
 }
 
+let previousButtonTop = 0;
+
 function addProgressIMPL(el, NAV_HEIGHT, markers) {
   let screenHeight = window.innerHeight;
   let elHeight = el.offsetHeight;
+  const content = el.querySelector('.firefly-model-showcase-content');
+  const initialContentHeight = content.clientHeight;
   window.addEventListener(
     'resize',
     throttle(() => {
@@ -36,25 +40,12 @@ function addProgressIMPL(el, NAV_HEIGHT, markers) {
 
   function updateProgress() {
     const rect = el.getBoundingClientRect();
-    const buttonRect = el.querySelector('.firefly-model-showcase-content .action-area a:first-of-type').getBoundingClientRect();
-    const buttonProgress = screenHeight - buttonRect.top;
-    const content = el.querySelector('.firefly-model-showcase-content');
-
     // how much of the el already entered from bottom
     const enterProgress = clamp((screenHeight - rect.top) / elHeight, 0, 1);
     // how much of the el already exited from top (gnav)
     const exitProgress = clamp((-rect.top + NAV_HEIGHT) / elHeight, 0, 1);
-
-    if (buttonProgress < 100) {
-      content.classList.add('opacity-gated');
-    } else {
-      content.classList.remove('opacity-gated');
-    }
-
     el.style.setProperty('--enter-progress', enterProgress * 100);
     el.style.setProperty('--exit-progress', exitProgress * 100);
-    el.style.setProperty('--button-progress', buttonProgress);
-
     if (markers.length) {
       markers.forEach((marker) => {
         const { name, threshold, type = 'exit' } = marker;
@@ -73,10 +64,18 @@ function addProgressIMPL(el, NAV_HEIGHT, markers) {
   window.addEventListener(
     'scroll',
     () => {
+      /* if content height changed due to additional spacing (e.g. dylan text spacing),
+      skip the parallax animation */
+      if (initialContentHeight !== content.clientHeight) return;
+      const buttonRect = el.querySelector('.firefly-model-showcase-content .action-area a:first-of-type').getBoundingClientRect();
+      const currentButtonTop = buttonRect.top;
+      // if the button is below the fold, skip the parallax animation
+      if (previousButtonTop - screenHeight > 0 && !(previousButtonTop < 0)) return;
       if (!ticking) {
         requestAnimationFrame(updateProgress);
         ticking = true;
       }
+      previousButtonTop = currentButtonTop;
     },
     { passive: true },
   );
