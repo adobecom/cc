6619b5ce688e1e291a8efc58fa34bb2f27ca33ed
himani-kuraware <hkuraware@adobe.com>
2026-03-31 16:40:13 +0530

    tab index

--- creativecloud/blocks/prm-yt-gallery/prm-yt-gallery.js
+++ creativecloud/blocks/prm-yt-gallery/prm-yt-gallery.js
@@ -295,7 +295,7 @@ const createEditButton = (buttonText) => {
 // Creates the info overlay with text container.
 const createInfoOverlay = () => {
   const overlay = createTag('div', { class: CLASSES.INFO_OVERLAY });
-  const overlayText = createTag('p', { class: CLASSES.OVERLAY_TEXT, tabindex: '-1', 'aria-hidden': 'true' });
+  const overlayText = createTag('p', { class: CLASSES.OVERLAY_TEXT, tabindex: '-1' });
   overlay.append(overlayText);
   return overlay;
 };
