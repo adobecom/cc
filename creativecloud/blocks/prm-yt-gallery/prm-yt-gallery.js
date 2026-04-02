b85de1dfa0f0c435caf6ac2b52c824a0d21f36c7
himani-kuraware <hkuraware@adobe.com>
2026-04-02 19:05:31 +0530

    added aria-hidden true when show-info-overlay

--- creativecloud/blocks/prm-yt-gallery/prm-yt-gallery.js
+++ creativecloud/blocks/prm-yt-gallery/prm-yt-gallery.js
@@ -386,7 +386,7 @@ const showInfoOverlay = (card, video, closeOverlayButton) => {
   if (video) video.pause();
   if (closeOverlayButton) {
     // closeOverlayButton.tabindex = 0;
-    // setAriaHidden(closeOverlayButton, false);
+    setAriaHidden(closeOverlayButton, true);
     closeOverlayButton.focus();
   }
 };
