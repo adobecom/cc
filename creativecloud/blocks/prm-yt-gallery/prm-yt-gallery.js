9c257735d8520302988f0f7a427cc7c6b5adcf6f
himani-kuraware <hkuraware@adobe.com>
2026-04-03 18:10:02 +0530

    removed role

--- creativecloud/blocks/prm-yt-gallery/prm-yt-gallery.js
+++ creativecloud/blocks/prm-yt-gallery/prm-yt-gallery.js
@@ -311,7 +311,7 @@ const createShimmerCard = (buttonText) => {
   const card = createTag('div', {
     class: `${CLASSES.CARD} ${CLASSES.SHIMMER}`,
     tabindex: '0',
-    role: 'presentation',
+    // role: 'presentation',
   });
   const cardInner = createTag('div', { class: CLASSES.CARD_INNER });
   const imageWrapper = createTag('div', { class: CLASSES.IMAGE_WRAPPER });
