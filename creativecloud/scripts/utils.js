d8e59cd82fdeffe041e89dac487028f0eafa93f9
Blaine Gunn <Blainegunn@gmail.com>
2026-04-09 20:41:48 -0600

    Revert "MWPW-191633: Adds to setLibs array to allow the use of milolibs= on s…" (#972)
    This reverts commit e768f0b4eee8f7f48867e6fd479a196900055385.

--- creativecloud/scripts/utils.js
+++ creativecloud/scripts/utils.js
@@ -169,13 +169,16 @@ export const [setLibs, getLibs] = (() => {
         return libs;
       }
       const { hostname } = window.location;
-      if (!['.aem.', '.hlx.', '.stage.', 'localhost', '.da.'].some((i) => hostname.includes(i))) {
+      if (!hostname.includes('hlx.page')
+        && !hostname.includes('hlx.live')
+        && !hostname.includes('aem.page')
+        && !hostname.includes('aem.live')
+        && !hostname.includes('localhost')) {
         libs = prodLibs;
         return libs;
       }
       const branch = new URLSearchParams(window.location.search).get('milolibs') || 'main';
       if (branch === 'local') { libs = 'http://localhost:6456/libs'; return libs; }
-      if (branch === 'main' && hostname.includes('.stage.')) return '/libs';
       const env = hostname.includes('.hlx.') ? 'hlx' : 'aem';
       if (branch.indexOf('--') > -1) { libs = `https://${branch}.${env}.live/libs`; return libs; }
       libs = `https://${branch}--milo--adobecom.${env}.live/libs`;
